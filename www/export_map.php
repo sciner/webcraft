edbf9d54ca0ef0569d125ef30c694a053fb4681d
sciner <sciner@ya.ru>
2021-07-13 22:46:56 +0300

    remove unused file

--- www/export_map.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    $f = file_get_contents(__DIR__.'/map_demo.json');
-    $f = json_decode($f);
-    foreach((array)$f->modifiers as $k => $v) {
-        file_put_contents(__DIR__ . '/../server/world/demo/' . $k . '.json', json_encode($v, JSON_UNESCAPED_UNICODE));
-    }
