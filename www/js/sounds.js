551571a82ee1ed45aa38717cb55481ddcb128d39
SCINER <sciner@ya.ru>
2021-07-21 20:57:48 +0300

    Add sounds.json

--- www/js/sounds.js
+++ www/js/sounds.js
@@ -1,64 +1,32 @@
 class Sounds {
 
     constructor() {
+        var that = this;
         this.tags = {};
-        this.tags['wood'] = {
-            list: [
-                new Howl({src: ["/sounds/wood1.mp3"]}),
-                new Howl({src: ["/sounds/wood2.mp3"]}),
-                new Howl({src: ["/sounds/wood3.mp3"]}),
-                new Howl({src: ["/sounds/wood4.mp3"]})
-            ],
-            play: function() {
-                var i = Math.floor(Math.random() * this.list.length);
-                this.list[i].play();
+        loadJSON('../sounds.json', function(json) {
+            for(var sound of json) {
+                that.add(sound);
             }
-        };
-        this.tags['grass'] = {
-            list: [
-                new Howl({src: ["/sounds/grass1.mp3"]}),
-                new Howl({src: ["/sounds/grass2.mp3"]}),
-                new Howl({src: ["/sounds/grass3.mp3"]}),
-                new Howl({src: ["/sounds/grass4.mp3"]})
-            ],
-            play: function() {
-                var i = Math.floor(Math.random() * this.list.length);
-                this.list[i].play();
-            }
-        };
-        this.tags['stone'] = {
-            list: [
-                new Howl({src: ["/sounds/stone1.mp3"]}),
-                new Howl({src: ["/sounds/stone2.mp3"]}),
-                new Howl({src: ["/sounds/stone3.mp3"]}),
-                new Howl({src: ["/sounds/stone4.mp3"]})
-            ],
-            play: function() {
-                var i = Math.floor(Math.random() * this.list.length);
-                this.list[i].play();
-            }
-        };
-        this.tags['gravel'] = {
-            list: [
-                new Howl({src: ["/sounds/gravel1.mp3"]})
-            ],
-            play: function() {
-                var i = Math.floor(Math.random() * this.list.length);
-                this.list[i].play();
-            }
-        };
-        this.tags['click'] = {
-            list: [
-                new Howl({src: ["/sounds/click.mp3"]})
-            ],
-            play: function() {
-                this.list[0].play();
-            }
-        };
+        });
     }
 
-    play(tag) {
-        this.tags[tag].play();
+    add(item) {
+        for(var action of ['dig', 'place']) {
+            for(var i in item[action]) {
+                var src = item[action][i];
+                item[action][i] = new Howl({src: [src]})
+            }
+        }
+        this.tags[item.type] = item;
+    }
+
+    play(tag, action) {
+        if(!this.tags.hasOwnProperty(tag)) {
+            return;
+        }
+        const list = this.tags[tag][action];
+        var i = Math.floor(Math.random() * list.length);
+        list[i].play();
     }
 
 }
