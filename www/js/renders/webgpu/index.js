3ce73df4d88a6027d4ea8a103e0d2ecf4a905fdf
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-08-13 15:55:35 +0300

    feat: types, base rendere class
    Former-commit-id: a885e71b094f1861953bc077778fce0a76b5bf10

--- www/js/renders/webgpu/index.js
+++ www/js/renders/webgpu/index.js
@@ -1,6 +1,42 @@
 //@ts-check
-export default class WebGPURenderer {
-    
+import BaseRenderer from "../BaseRenderer.js";
+
+export default class WebGPURenderer extends BaseRenderer{
+    constructor(view, options) {
+        super(view, options);
+        /**
+         *
+         * @type {GPUDevice} {null}
+         */
+        this.device = null;
+        /**
+         *
+         * @type {GPUAdapter}
+         */
+        this.adapter = null;
+        /**
+         *
+         * @type {GPUCanvasContext}
+         */
+        this.context = null;
+    }
+
+    async init() {
+        this.adapter = await navigator.gpu.requestAdapter();
+        this.device = await this.adapter.requestDevice();
+        this.context = this.view.getContext('webgpu');
+    }
+
+    resize(w, h) {
+        super.resize(w, h);
+
+        this.view.width = w;
+        this.view.height = h;
+    }
+
+    _configure() {
+        super._configure();
+    }
 }
 
 /**
