ac84264de5e6b4b51c96d19df855df924dd0ab19
sciner <sciner@ya.ru>
2021-08-15 01:54:19 +0300

    Fix block destroy animation effect

--- www/js/particles/block_destroy.js
+++ www/js/particles/block_destroy.js
@@ -1,6 +1,6 @@
 import {TX_CNT, DIRECTION, NORMALS, Vector, Color} from '../helpers.js';
 import {BLOCK} from '../blocks.js';
-import {push_plane} from '../blocks_func.js';
+import {push_plane, QUAD_FLAGS, MULTIPLY} from '../blocks_func.js';
 import GeometryTerrain from "../geometry_terrain.js";
 
 export default class Particles_Block_Destroy {
@@ -15,12 +15,19 @@ export default class Particles_Block_Destroy {
         let cell        = chunk.map.cells[pos.x - chunk.coord.x][pos.z - chunk.coord.z];
         this.yaw        = -Game.world.localPlayer.angles[2];
         this.life       = .5;
-        let lm          = new Color(cell.biome.dirt_color.r, cell.biome.dirt_color.g, cell.biome.dirt_color.b, 0);
+        let lm          = MULTIPLY.COLOR.WHITE;
         let n           = NORMALS.UP; // normal for lithning
         this.texture    = BLOCK.fromId(block.id).texture;
+        let flags       = 0;
+        let sideFlags   = 0;
+        let upFlags     = QUAD_FLAGS.NORMAL_UP;
         if(typeof this.texture != 'function') {
             this.life = 0;
             return;
+        }
+        if(block.id == BLOCK.DIRT.id) {
+            lm          = cell.biome.dirt_color;
+            sideFlags   = QUAD_FLAGS.MASK_BIOME;
         }
         let c           = BLOCK.calcTexture(this.texture(this, null, 1, null, null, null, DIRECTION.FORWARD)); // полная текстура
         this.pos        = new Vector(
@@ -37,12 +44,12 @@ export default class Particles_Block_Destroy {
             // random tex coord (случайная позиция в текстуре)
             let cx = c[0] + Math.random() * (half * 3);
             let cy = c[1] + Math.random() * (half * 3);
-            let c_half = [cx, cy, cx + half, cy + half];
-            // случаная позиция частицы (в границах блока)
+            let c_half = [cx - c[2] / 2 + half / 2, cy - c[3] / 2 + half / 2, half, half];
+            // случайная позиция частицы (в границах блока)
             let x = (Math.random() - Math.random()) * .5;
             let y = (Math.random() - Math.random()) * .5;
             let z = (Math.random() - Math.random()) * .5;
-            push_plane(this.vertices, x, y, z, c_half, lm, n, true, false, sz, sz, null);
+            push_plane(this.vertices, x, y, z, c_half, lm, n, true, false, sz, sz, null, flags | upFlags | sideFlags);
             let p = {
                 x:              x,
                 y:              y,
@@ -56,7 +63,7 @@ export default class Particles_Block_Destroy {
             p.z = p.z / d * p.speed;
             this.particles.push(p);
         }
-        this.buffer = new GeometryTerrain(GeometryTerrain.convertFrom12(this.vertices));
+        this.buffer = new GeometryTerrain(new Float32Array(this.vertices));
     }
 
     // Draw
@@ -68,7 +75,7 @@ export default class Particles_Block_Destroy {
         let idx = 0;
         for(let p of this.particles) {
             for(let i = 0; i < p.vertices_count; i++) {
-                let j = (idx + i) * 12;
+                let j = (idx + i) * GeometryTerrain.strideFloats;
                 this.vertices[j + 0] += p.x * delta * p.speed;
                 this.vertices[j + 1] += p.z * delta * p.speed;
                 this.vertices[j + 2] += (delta / 1000) * p.gravity;
@@ -77,7 +84,7 @@ export default class Particles_Block_Destroy {
             p.gravity -= delta / 250000;
         }
         //
-        this.buffer.updateInternal(GeometryTerrain.convertFrom12(this.vertices));
+        this.buffer.updateInternal(this.vertices);
         //
         mat4.identity(modelMatrix);
         let a_pos = new Vector(this.pos.x - Game.shift.x, this.pos.z - Game.shift.z, this.pos.y - Game.shift.y);
