4fb983faf9bfa08da12ac3854525473b71087178
SCINER <sciner@ya.ru>
2021-08-19 18:16:25 +0300

    3dchunk
    Former-commit-id: 501b20922d2bbcd75840eb13e40ad97dd56cde47

--- www/js/controller.js
+++ www/js/controller.js
@@ -39,7 +39,7 @@ let gameCtrl = function($scope, $timeout) {
     $scope.settings = {
         form: {
             hd: false,
-            texture_pack: 'terrain_hd',
+            texture_pack: 'terrain_kenney',
             mipmap: false
         },
         save: function() {
