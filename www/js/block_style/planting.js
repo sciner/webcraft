c1e0ee51f7b68da9e25870627e195b4baca8b6d3
SCINER <sciner@ya.ru>
2021-10-19 20:45:03 +0300

    Fixed draw planting

--- www/js/block_style/planting.js
+++ www/js/block_style/planting.js
@@ -1,7 +1,9 @@
 import {MULTIPLY, DIRECTION, QUAD_FLAGS} from '../helpers.js';
 import { default as push_plane_style } from './plane.js';
+import {CHUNK_SIZE_Z} from "../blocks.js";
 
 const push_plane = push_plane_style.getRegInfo().func;
+let randoms = [];
 
 // Растения
 export default class style {
@@ -29,7 +31,15 @@ export default class style {
         if(chunk.coord) {
             ao = BLOCK.applyLight2AO(lightmap, ao, x, Math.round(y), z);
         }
-        push_plane(vertices, x, y, z, c, lm, ao, true, true, undefined, undefined, undefined, flags);
+        let sz = 1 / 1.41;
+        let index = x * CHUNK_SIZE_Z + z;
+        if(!randoms[index]) {
+            randoms[index] = Math.random();
+        }
+        let r = randoms[index] * 4/16 - 2/16;
+        x += 0.5 - 0.5 / 1.41 + r;
+        z += 0.5 - 0.5 / 1.41 + r;
+        push_plane(vertices, x, y, z, c, lm, ao, true, true, sz, undefined, sz, flags);
     }
 
 }
