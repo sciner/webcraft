4e93e1c0d22aa1e1312469ba9c45aa96c931584d
SCINER <sciner@ya.ru>
2021-11-11 20:50:52 +0300

    Decrease crosshair size

--- www/resource_packs/default/shaders/webgl/vertex.glsl
+++ www/resource_packs/default/shaders/webgl/vertex.glsl
@@ -49,11 +49,12 @@ void main() {
         v_normal = normalize(cross(a_axisX, a_axisY));
     }
 
-    // Crosshair
+	// Crosshair
+    float cm = 0.00065;
     if(u_resolution.x > u_resolution.y) {
-        crosshair = vec4(0., 0., u_resolution.x * 0.001, u_resolution.x * 0.001 * 7.);
+        crosshair = vec4(0., 0., u_resolution.x * cm, u_resolution.x * cm * 7.);
     } else {
-        crosshair = vec4(0., 0., u_resolution.y * 0.001, u_resolution.y * 0.001 * 7.);
+        crosshair = vec4(0., 0., u_resolution.y * cm, u_resolution.y * cm * 7.);
     }
 
     v_normal.yz = v_normal.zy;
