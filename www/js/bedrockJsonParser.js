fea00e3115ea828c01f180cfc35a3253ad7bbb12
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-11-20 13:43:01 +0300

    fix: bedrock loader

--- www/js/bedrockJsonParser.js
+++ www/js/bedrockJsonParser.js
@@ -1,70 +1,34 @@
 //@ts-check
 
-import { NORMALS, Helpers } from './helpers.js';
 import { SceneNode } from "./SceneNode.js";
 import GeometryTerrain from "./geometry_terrain.js";
 
 const {mat4, vec3, quat} = glMatrix;
+const SCALE_RATIO = 4 / 16;
 
 const computeMatrix = mat4.create();
 const computePos = vec3.create();
+const computePivot = vec3.create();
 const computeScale = vec3.create();
 const computeRot = quat.create();
-
-const BOX_TEMPLATE = new Float32Array([
-    // Top
-    0.0, 0.0, 1.0, 8/64, 0, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-    1.0, 0.0, 1.0, 16/64, 0, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-    1.0, 1.0, 1.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-    1.0, 1.0, 1.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-    0.0, 1.0, 1.0, 8/64, 8/64, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-    0.0, 0.0, 1.0, 8/64, 0, 0, 0, 0, 0, NORMALS.UP.x, NORMALS.UP.y, NORMALS.UP.z,
-
-    // Bottom
-    0.0, 0.0, 0.0, 16/64, 0, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-    0.0, 1.0, 0.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-    1.0, 1.0, 0.0, 24/64, 8/64, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-    1.0, 1.0, 0.0, 24/64, 8/64, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-    1.0, 0.0, 0.0, 24/64, 0, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-    0.0, 0.0, 0.0, 16/64, 0, 0, 0, 0, 0, NORMALS.DOWN.x, NORMALS.DOWN.y, NORMALS.DOWN.z,
-
-    // Front
-    0.0, 0.0, 1.0, 8/64, 8/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-    0.0, 0.0, 0.0, 8/64, 16/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-    1.0, 0.0, 0.0, 16/64, 16/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-    1.0, 0.0, 0.0, 16/64, 16/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-    1.0, 0.0, 1.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-    0.0, 0.0, 1.0, 8/64, 8/64, 0, 0, 0, 0, NORMALS.FORWARD.x, NORMALS.FORWARD.y, NORMALS.FORWARD.z,
-
-    // Rear
-    0.0, 1.0, 1.0, 24/64, 8/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-    1.0, 1.0, 1.0, 32/64, 8/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-    1.0, 1.0, 0.0, 32/64, 16/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-    1.0, 1.0, 0.0, 32/64, 16/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-    0.0, 1.0, 0.0, 24/64, 16/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-    0.0, 1.0, 1.0, 24/64, 8/64, 0, 0, 0, 0, NORMALS.BACK.x, NORMALS.BACK.y, NORMALS.BACK.z,
-
-    // Right
-    0.0, 0.0, 1.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-    0.0, 1.0, 1.0, 24/64, 8/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-    0.0, 1.0, 0.0, 24/64, 16/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-    0.0, 1.0, 0.0, 24/64, 16/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-    0.0, 0.0, 0.0, 16/64, 16/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-    0.0, 0.0, 1.0, 16/64, 8/64, 0, 0, 0, 0, NORMALS.RIGHT.x, NORMALS.RIGHT.y, NORMALS.RIGHT.z,
-
-    // Left
-    1.0, 0.0, 1.0, 8/64, 8/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-    1.0, 0.0, 0.0, 8/64, 16/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-    1.0, 1.0, 0.0, 0/64, 16/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-    1.0, 1.0, 0.0, 0/64, 16/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-    1.0, 1.0, 1.0, 0/64, 8/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-    1.0, 0.0, 1.0, 8/64, 8/64, 0, 0, 0, 0, NORMALS.LEFT.x, NORMALS.LEFT.y, NORMALS.LEFT.z,
-
-]);
 
 let lm = {r : -1, g : -1, b : -1};
 // let lm = {r : 0, g : 0, b : 0};
 let ao = [0, 0, 0, 0];
+
+function convertVect (a, b, flip = false) {
+    if (flip) {
+        b[0] = a[0] / 16;
+        b[1] = a[2] / 16;
+        b[2] = a[1] / 16;
+    } else {
+        b[0] = a[0] / 16;
+        b[1] = a[1] / 16;
+        b[2] = a[2] / 16;
+    }
+
+    return b;
+}
 
 function fillCube({ matrix, rot, pos, scale, uvPoint = [0,0], inflate = 0}, target) {
     // let xX = matrix[0], xY = matrix[1], xZ = matrix[2];
@@ -137,29 +101,6 @@ function fillCube({ matrix, rot, pos, scale, uvPoint = [0,0], inflate = 0}, targ
         lm.r, lm.g, lm.b,
         ...ao, flags);
 
-
-    const data = BOX_TEMPLATE.slice();
-    const q = rot;
-
-    for(let i = 0; i < 36 * 12; i += 12) {
-        const pos = data.subarray(i, i + 3);
-        const uv = data.subarray(i + 3, i + 5);
-        const normal = data.subarray(i + 3 + 2 + 4, i + 3 + 2 + 4 + 3);
-
-        pos[0] += inflate * normal[0];
-        pos[1] += inflate * normal[1];
-        pos[2] += inflate * normal[2];
-
-        vec3.transformMat4(pos, pos, matrix);
-        vec3.transformQuat(normal, normal, q);
-
-        //const uvIndex = i / 12;
-        //uv[0] = uvPoint[0];
-        //uv[1] = uvPoint[1];
-    }
-
-    const target2 = GeometryTerrain.convertFrom12(data)
-
     // target.push(...target2);
 
     return target;
@@ -174,21 +115,56 @@ function fillCube({ matrix, rot, pos, scale, uvPoint = [0,0], inflate = 0}, targ
 function decodeCubes(cubes, description, offset = null) {
     const data = [];
 
-    computePos.set([0,0,0]);
-    computeScale.set([1, 1, 1]);
-    computeRot.set([0, 0, 0, 1]);
-
     for(let c of cubes) {
-        c.origin && computePos.set(c.origin);
-        c.size && computeScale.set(c.size);
+        const {
+            origin = [0,0,0],
+            size = [1,1,1],
+            pivot = [0,0,0]
+        } = c;
+        
+        vec3.set(computePivot,
+            pivot[0] * SCALE_RATIO,
+            pivot[1] * SCALE_RATIO,
+            pivot[2] * SCALE_RATIO
+        );
 
-        offset && vec3.subtract(computePos, computePos, offset);
+        vec3.set(computeScale,
+            size[0] * SCALE_RATIO,
+            size[1] * SCALE_RATIO,
+            size[2] * SCALE_RATIO
+        );
 
-        c.rotation && quat.fromEuler(computeRot, ...c.rotation);
+        vec3.set(computePos, 
+            (origin[0] - pivot[0]) * SCALE_RATIO,
+            (origin[1] - pivot[1]) * SCALE_RATIO,
+            (origin[2] - pivot[2]) * SCALE_RATIO
+        );  
 
-        mat4.fromRotationTranslationScale(computeMatrix, computeRot, computePos, computeScale);
-        //mat4.multiplyScalar(computeMatrix, computeMatrix, 1 / 36);
+        if (c.rotation) {
+            quat.fromEuler(
+                computeRot,
+                c.rotation[0],
+                c.rotation[1],
+                -c.rotation[2] // WHY???
+            );
+        } else {
+            computeRot.set([0,0,0,1])
+        }
 
+        mat4.fromRotationTranslationScale(
+            computeMatrix, 
+            quat.create(),
+            computePos,
+            computeScale,
+        );
+
+        mat4.multiply(computeMatrix, mat4.fromQuat(mat4.create(), computeRot), computeMatrix);
+
+        computeMatrix[12] += computePivot[0];
+        computeMatrix[13] += computePivot[1];
+        computeMatrix[14] += computePivot[2];
+    
+    
         fillCube({
                 matrix: computeMatrix,
                 uvPoint: [c.uv[0] / description.texture_width, c.uv[1] / description.texture_height],
@@ -247,7 +223,6 @@ export function decodeJsonGeometryTree(json) {
         const sceneNode = new SceneNode();
 
         if (!node.parent) {
-            // quat.rotateX(sceneNode.quat, sceneNode.quat, Math.PI / 2);
             root.addChild(sceneNode);
         }
 
@@ -259,11 +234,21 @@ export function decodeJsonGeometryTree(json) {
         }
 
         if (node.rotation) {
-            quat.fromEuler(sceneNode.quat, ...node.rotation)
+            // MAGIC, we MUST flip axis
+            quat.fromEuler(
+                sceneNode.quat,
+                node.rotation[0],
+                node.rotation[2],
+                node.rotation[1]
+            )
         }
 
         if (node.pivot) {
-            sceneNode.pivot.set(node.pivot);
+            sceneNode.pivot.set([
+                node.pivot[0] * SCALE_RATIO,
+                node.pivot[2] * SCALE_RATIO,
+                node.pivot[1] * SCALE_RATIO
+            ]);
         }
 
         sceneNode.updateMatrix();
