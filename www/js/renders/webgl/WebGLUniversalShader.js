384271e364dd0937c1265d6a1d809f0a1de15bdd
Konstantin Timoshenko <rondo.devil@gmail.com>
2022-01-19 20:37:25 +0300

    fix: Universal shader uniforms loader

--- www/js/renders/webgl/WebGLUniversalShader.js
+++ www/js/renders/webgl/WebGLUniversalShader.js
@@ -1,6 +1,74 @@
 import { BaseShader } from "../BaseRenderer.js";
 import WebGLRenderer from "./index.js";
 
+const p = WebGLRenderingContext.prototype;
+
+/**
+ * @typedef UnformLoaderInfo
+ * @property {string} name
+ * @property {string} trimmedName
+ * @property {WebGLUniformLocation} location
+ * @property {WebGLActiveInfo} info
+ * @property {*} value
+ * @property {string} func
+ * @property {string} type
+ * @property {boolean} isolate isolate inoform, it will not upploaded from GU
+ */
+
+/**
+ * @typedef AttrLoaderInfo
+ * @property {string} name
+ * @property {string} trimmedName
+ * @property {number} location
+ * @property {WebGLActiveInfo} info
+ */
+
+const U_LOADER = {
+    'uniformMatrix4fv': (gl, ptr, value) => gl.uniformMatrix4fv(ptr, false, value)
+}
+
+const GL_TYPE_FUNC = {
+    [p.BOOL] : {
+        type : 'int',
+        func : 'uniform1i'
+    },
+
+    [p.FLOAT] : {
+        type : 'float',
+        func : 'uniform1f' 
+    },
+
+    [p.SAMPLER_CUBE]: {
+        type : 'int',
+        func : 'uniform1i' 
+    },
+
+    [p.SAMPLER_2D] : {
+        type : 'int',
+        func : 'uniform1i' 
+    },
+
+    [p.FLOAT_VEC2] : {
+        type: 'vec2',
+        func: 'uniform2fv'
+    },
+
+    [p.FLOAT_VEC3] : {
+        type: 'vec3',
+        func: 'uniform3fv'
+    },
+
+    [p.FLOAT_VEC4] : {
+        type: 'vec4',
+        func: 'uniform4fv'
+    },
+
+    [p.FLOAT_MAT4] : {
+        type: 'mat4',
+        func: U_LOADER['uniformMatrix4fv']
+    }
+}
+
 export class WebGLUniversalShader extends BaseShader {
     /**
      * 
@@ -10,17 +78,29 @@ export class WebGLUniversalShader extends BaseShader {
     constructor (context, options) {
         super(context, options);
 
+        this.useGlobalUniforms = options && options.useGlobalUniforms === false ? false : true;
+
         /**
          * @type {WebGLProgram}
          */
-        this.program = context.createProgram(options.code, options.defines || []);
+        this.program = context.createProgram(options.code, options.defines || {});
 
         /**
-         * @type {[key: string] : {location: number, info: WebGLActiveInfo, value: null }}
+         * @type {AttrLoaderInfo[]}
+         */
+        this._attrsFlat = [];
+        /**
+         * @type {UnformLoaderInfo[]}
+         */
+        this._uniformsFlat = [];
+
+        /**
+         * @type {{[key: string] : AttrLoaderInfo }}
          */
         this.attrs = {};
+
         /**
-         * @type {[key: string] : {location: WebGLUniformLocation,  info: WebGLActiveInfo, value: null }}
+         * @type {{[key: string] : UnformLoaderInfo}}
          */
         this.uniforms = {};
 
@@ -43,10 +123,16 @@ export class WebGLUniversalShader extends BaseShader {
         for(let i = 0; i < attrsCount; i ++) {
             const attr = gl.getActiveAttrib(p, i);
 
-            this.attrs[attr.name] = {
+            const record = this.attrs[attr.name] = {
                 location: gl.getAttribLocation(p, attr.name),
-                info: attr
+                info: attr,
+                name: attr.name,
+                trimmedName: attr.name.replace('a_', '')
             };
+
+            this.attrs[record.trimmedName] = record;
+
+            this._attrsFlat.push(record)
         }
 
         const uniformCount = gl.getProgramParameter(p, gl.ACTIVE_UNIFORMS);
@@ -54,10 +140,22 @@ export class WebGLUniversalShader extends BaseShader {
         for(let i = 0; i < uniformCount; i ++) {
             const uniform = gl.getActiveUniform(p, i);
 
-            this.uniforms[uniform.name] = {
+            const record = this.uniforms[uniform.name] = {
                 location: gl.getUniformLocation(p, uniform.name),
-                info: uniform
+                info: uniform,
+                name: uniform.name,
+                isolate: false,
+                trimmedName: uniform.name.replace('u_', '').replace('[]', '') // trim u_ and []
             };
+
+            if (uniform.type in GL_TYPE_FUNC) {
+                Object.assign(this.uniforms[uniform.name], GL_TYPE_FUNC[uniform.type]);
+            }
+
+            // for easy lookup
+            this.uniforms[uniform.trimmedName] = record;
+
+            this._uniformsFlat.push(record);
         }
     }
 
@@ -68,66 +166,51 @@ export class WebGLUniversalShader extends BaseShader {
         } = this.context;
 
         for(const name in uniforms) {
-            const value = uniforms[name];
-            let type = null;
-            let func = null;
-            
-            switch(typeof value) {
-                case 'boolean': {
-                    type = 'bool';
-                    func = 'uniform1f';
-                    break;
-                }
-
-                case 'object': {
-                    // matrix
-                    if (value.length === 16) {
-                        type = `mat4`;
-                        func = `uniformMatrix4fv`;
-                        break;
-                    }
-
-                    if (Array.isArray(value)) {
-                        type = `vec${value.length}`;
-                        func = `uniform${value.length}fv`;
-                        break;
-                    }
-
-                    type = 'vec3';
-                    func = 'uniform3fv';
-                    break;
-                }
-
-                case 'number': {
-                    type = 'float';
-                    func = 'uniform1f';
-                    break;
-                }
-                
-                default: {
-                    throw 'Unsupported uniform type ' + (typeof value);
-                }
+            if (!this.uniforms[name]) {
+                console.warn('Unknow uniform location: ' + name);
+                continue;
             }
 
-            if (this.uniforms[name]) {
-                throw 'Unknow uniform location' + name;
+            // we already know loaders
+            if (this.uniforms[name].func) {
+                this.uniforms[name].value = uniforms[name];
+                continue;
             }
 
-            Object.assign(this.uniforms[name], {
-                type,
-                func,
-                value
-            });
+            console.log('Unknow uniform loader for', name, this.uniforms[name]);
         }
     }
 
     _applyUniforms() {
-        const { gl } = this.context;
+        const { gl, globalUniforms } = this.context;
         gl.useProgram(this.program);
 
         // Bind custom uniforms
-        for(const name in this.uniforms) {
-            const unf = this.uniforms[name];
+        for(const unf of this._uniformsFlat) {
+            const name = unf.name;
+            const trimmed = unf.trimmedName;
+
+            let value = unf.value;
+
+            // try upload from GU
+            // redefine base value
+            if (this.useGlobalUniforms && !unf.isolate) {
+                if (trimmed in globalUniforms) {
+                    value = globalUniforms[trimmed];
+                } else if (name in globalUniforms) {
+                    value = globalUniforms[name];
+                }
+            }
+
+            if (typeof value === 'undefined') {
+                console.log('Uniform value missing for ', name, this.constructor.name);
+                continue;
+            }
+            
+            if (typeof unf.func === 'function') {
+                unf.func(gl, unf.location, unf.value);
+                continue;
+            }
 
             if (!unf.func || !(unf.func in gl)) {
                 continue;
@@ -160,6 +243,10 @@ export class WebGLUniversalShader extends BaseShader {
         gl.useProgram(this.program);
 
         this.update();
+    }
+
+    unbind() {
+
     }
 
     update() {
