b55f5e3656245b7e28281b78c2a9009ba7a489a7
sciner <sciner@ya.ru>
2022-01-20 23:25:45 +0300

    painting

--- www/js/particles/painting.js
+++ www/js/particles/painting.js
@@ -1,6 +1,7 @@
 import {DIRECTION, MULTIPLY, Vector} from '../helpers.js';
 import GeometryTerrain from "../geometry_terrain.js";
 import {BLOCK} from "../blocks.js";
+import { getChunkAddr, CHUNK_SIZE_X, CHUNK_SIZE_Y, CHUNK_SIZE_Z } from "../chunk.js";
 // import {fillCube} from "../bedrockJsonParser.js";
 
 const {mat4} = glMatrix;
@@ -22,18 +23,23 @@ export class Particles_Painting {
         let flags           = 0, sideFlags = 0, upFlags = 0;
         let c               = [2/4, 2/4, 1/4, 1/4];
 
+        this.pos            = new Vector(params.aabb[0], params.aabb[1], params.aabb[2]);
+        // this._chunk_addr    = new Vector(0, 0, 0);
+
         //
         this.lightTex       = null;
         this.life           = 1.0;
         this.vertices       = [];
 
+        // console.log(params);
+
         for(let shape of [params.aabb]) {
-            let x1          = shape[0];
-            let y1          = shape[1];
-            let z1          = shape[2];
-            let x2          = shape[3];
-            let y2          = shape[4];
-            let z2          = shape[5];
+            let x1          = shape[0] - this.pos.x;
+            let y1          = shape[1] - this.pos.y;
+            let z1          = shape[2] - this.pos.z;
+            let x2          = shape[3] - this.pos.x;
+            let y2          = shape[4] - this.pos.y;
+            let z2          = shape[5] - this.pos.z;
             let xw          = x2 - x1; // ширина по оси X
             let yw          = y2 - y1; // ширина по оси Y
             let zw          = z2 - z1; // ширина по оси Z
@@ -53,13 +59,13 @@ export class Particles_Painting {
                 0, -zw, 0,
                 c[0], c[1], c[2] * xw, c[3] * zw,
                 lm.r, lm.g, lm.b, flags);
-            // South | Forward | z++ (XZY)
+            // South | Forward | z-- (XZY)
             this.vertices.push(xpos, zpos - zw/2, y_bottom + yw/2,
                 xw, 0, 0,
                 0, 0, yw,
                 c[0], c[1], c[2] * xw, -c[3] * yw,
                 lm.r, lm.g, lm.b, flags | sideFlags);
-            // North | Back | z--
+            // North | Back | z++
             this.vertices.push(xpos, zpos + zw/2, y_bottom + yw/2,
                 xw, 0, 0,
                 0, 0, -yw,
@@ -83,15 +89,20 @@ export class Particles_Painting {
 
     }
 
+    /*
+    get chunk_addr() {
+        return getChunkAddr(this.pos, this._chunk_addr);
+    }
+
     updateLightTex(render) {
-        /*const chunk = render.world.chunkManager.getChunk(this.chunk_addr);
+        const chunk = render.world.chunkManager.getChunk(this.chunk_addr);
         if (!chunk) {
             return;
         }
         this.chunk = chunk;
         this.lightTex = chunk.getLightTexture(render.renderBackend);
-        */
     }
+    */
 
     // Draw
     draw(render, delta) {
@@ -102,7 +113,7 @@ export class Particles_Painting {
         render.renderBackend.drawMesh(
             this.buffer,
             this.material,
-            null,
+            this.pos,
             this.modelMatrix
         );
 
