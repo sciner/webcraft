d64202153d0310b41f7cf47a1f73050fb9fcc47b
sciner <sciner@ya.ru>
2022-02-19 18:40:01 +0300

    Added azalea and flowering azalea

---
+++ www/js/block_style/azalea.js
@@ -0,0 +1,165 @@
+import {DIRECTION, Vector} from '../helpers.js';
+import {BLOCK} from "../blocks.js";
+import {AABB, AABBSideParams, pushAABB} from '../core/AABB.js';
+import glMatrix from "../../vendors/gl-matrix-3.3.min.js"
+
+const {mat4} = glMatrix;
+
+const w = 1;
+const h = 1;
+
+const WIDTH_INNER = 4/16;
+
+// Azalea
+export default class style {
+
+    // getRegInfo
+    static getRegInfo() {
+        return {
+            styles: ['azalea'],
+            func: this.func,
+            aabb: this.computeAABB
+        };
+    }
+
+    // computeAABB
+    static computeAABB(block, for_physic) {
+        let y = 0;
+        let aabb = new AABB();
+        aabb.set(
+            0 + .5 - w / 2,
+            y + .5,
+            0 + .5 - w / 2,
+            0 + .5 + w / 2,
+            y + 1,
+            0 + .5 + w / 2,
+        );
+        let aabb2 = new AABB();
+        aabb2.set(
+            0 + .5 - WIDTH_INNER / 2,
+            y + 0,
+            0 + .5 - WIDTH_INNER / 2,
+            0 + .5 + WIDTH_INNER / 2,
+            y + .5,
+            0 + .5 + WIDTH_INNER / 2,
+        );
+        return [aabb, aabb2];
+    }
+
+    // Build function
+    static func(block, vertices, chunk, x, y, z, neighbours, biome, unknown, matrix, pivot, force_tex) {
+
+        if(!block || typeof block == 'undefined' || block.id == BLOCK.AIR.id) {
+            return;
+        }
+
+        const c_up = BLOCK.calcMaterialTexture(block.material, DIRECTION.UP);
+        const c_side = BLOCK.calcMaterialTexture(block.material, DIRECTION.NORTH);
+        const c_down = BLOCK.calcMaterialTexture(block.material, DIRECTION.DOWN);
+
+        const chains = [];
+        chains.push({width: w, height: h, uv: [.5, .5], rot: Math.PI / 4, y: 0, translate: [w/2, 0, 0]});
+        chains.push({width: w, height: h, uv: [.5, .5], rot: -Math.PI / 4, y: 0, translate: [-w/2, 0, 0]});
+
+        const CHAIN_Y = y;
+
+        for(let chain of chains) {
+            const aabb_chain_middle = new AABB();
+            aabb_chain_middle.set(
+                x + .5 - chain.width/2,
+                CHAIN_Y + chain.y,
+                z + .5 - chain.width/2,
+                x + .5 + chain.width/2,
+                CHAIN_Y + chain.y + chain.height,
+                z + .5 + chain.width/2,
+            );
+            // Push vertices
+            matrix = mat4.create();
+            mat4.rotateY(matrix, matrix, chain.rot);
+            mat4.translate(matrix, matrix, chain.translate);
+            pushAABB(
+                vertices,
+                aabb_chain_middle,
+                pivot,
+                matrix,
+                {north:  new AABBSideParams(c_down, 0, 1)},
+                true,
+                new Vector(x, y, z)
+            );
+        }
+
+        //
+        matrix = mat4.create();
+
+        const aabb_up = new AABB();
+        aabb_up.set(
+            x,
+            y,
+            z,
+            x + 1,
+            y + 1,
+            z + 1
+        );
+
+        // Push vertices down
+        pushAABB(
+            vertices,
+            aabb_up,
+            pivot,
+            matrix,
+            {
+                up:     new AABBSideParams(c_up, 0, 1),
+                south:  new AABBSideParams(c_side, 0, 1),
+                north:  new AABBSideParams(c_side, 0, 1),
+                west:   new AABBSideParams(c_side, 0, 1),
+                east:   new AABBSideParams(c_side, 0, 1),
+            },
+            true,
+            new Vector(x, y, z)
+        );
+
+        /*
+        matrix = mat4.create();
+        // mat4.rotateY(matrix, matrix, ((block.rotate.x - 2) / 4) * -(2 * Math.PI));
+
+        // 2. Основная часть
+        // Textures
+        const c_top = BLOCK.calcMaterialTexture(block.material, DIRECTION.UP);
+        const c_side = BLOCK.calcMaterialTexture(block.material, DIRECTION.UP);
+        c_top[0] += (-.5 + 6/32) / 32;
+        c_top[1] += (-.5 + 24/32) / 32;
+        c_side[0] += (-.5 + 6/32) / 32;
+        c_side[1] += (-.5 + 11/32) / 32;
+        let aabb_down = new AABB();
+        aabb_down.set(
+            x + .5 - WIDTH/2,
+            y,
+            z + .5 - WIDTH/2,
+            x + .5 + WIDTH/2,
+            y + HEIGHT,
+            z + .5 + WIDTH/2,
+        );
+        // Push vertices
+        pushAABB(
+            vertices,
+            aabb_down,
+            pivot,
+            matrix,
+            {
+                up:     new AABBSideParams(c_top, 0, animations_side),
+                down:   new AABBSideParams(c_top, 0, animations_side),
+                south:  new AABBSideParams(c_side, 0, animations_side),
+                north:  new AABBSideParams(c_side, 0, animations_side),
+                west:   new AABBSideParams(c_side, 0, animations_side),
+                east:   new AABBSideParams(c_side, 0, animations_side),
+            },
+            true,
+            new Vector(x, y, z)
+        );
+        */
+
+        return null;
+
+    }
+
+}
