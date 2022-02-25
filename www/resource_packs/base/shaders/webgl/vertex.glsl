3fb0c70b42bb3900d265961d7a4ce38ddbcf2583
Konstantin Timoshenko <konstantin.t@deovr.com>
2022-02-25 22:02:25 +0300

    feat: animation interpolation
    clear

--- www/resource_packs/base/shaders/webgl/vertex.glsl
+++ www/resource_packs/base/shaders/webgl/vertex.glsl
@@ -10,14 +10,20 @@ void main() {
     #include<terrain_read_flags_vert>
 
     v_color = vec4(a_color, 1.0);
-    v_uvCenter = a_uvCenter;
+    v_uvCenter0 = a_uvCenter;
+    v_uvCenter1 = a_uvCenter;
+    v_animInterp = 0.0;
 
     // Animated textures
     if(v_color.b > 1.) {
         // v_color.b contain number of animation frames
         float disc = v_color.b - 1.;
-        float i = floor((u_time * v_color.b / 3.) / 1000.);
-        v_uvCenter.y += (abs(mod(i, disc * 2.) - disc)) / 32.;
+        float t = ((u_time * v_color.b / 3.) / 1000.);
+        float i = floor(t);
+
+        v_uvCenter0.y += (abs(mod(i, disc * 2.) - disc)) / 32.;
+        v_uvCenter1.y += (abs(mod(i + 1.0, disc * 2.) - disc)) / 32.;
+        v_animInterp = fract(t);
     }
 
     if (flagNormalUp == 1) {
@@ -30,9 +36,11 @@ void main() {
 
     vec3 pos = a_position + (a_axisX * a_quad.x) + (a_axisY * a_quad.y);
 
-    v_texcoord = v_uvCenter + (a_uvSize * a_quad);
+    v_texcoord0 = v_uvCenter0 + a_uvSize * a_quad;
+    v_texClamp0 = vec4(v_uvCenter0 - abs(a_uvSize * 0.5) + u_pixelSize * 0.5, v_uvCenter0 + abs(a_uvSize * 0.5) - u_pixelSize * 0.5);
 
-    v_texClamp = vec4(v_uvCenter - abs(a_uvSize * 0.5) + u_pixelSize * 0.5, v_uvCenter + abs(a_uvSize * 0.5) - u_pixelSize * 0.5);
+    v_texcoord1 = v_uvCenter1 + a_uvSize * a_quad;
+    v_texClamp1 = vec4(v_uvCenter1 - abs(a_uvSize * 0.5) + u_pixelSize * 0.5, v_uvCenter1 + abs(a_uvSize * 0.5) - u_pixelSize * 0.5);
 
     if(u_fogOn) {
         if (flagBiome == 0) {
