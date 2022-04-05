10318f0a651d9812a289ba007a6a35e47d8102c4
SCINER <sciner@ya.ru>
2022-04-05 18:24:28 +0300

    vilages

--- www/js/terrain_generator/chunk_cluster.js
+++ www/js/terrain_generator/chunk_cluster.js
@@ -3,8 +3,20 @@ import {Color, Vector, VectorCollector} from "../helpers.js";
 import {impl as alea} from '../../vendors/alea.js';
 
 const CLUSTER_PADDING   = 4;
+const STREET_WIDTH      = 15;
 const CLUSTER_SIZE      = new Vector(128, 128, 128);
 const temp_vec2         = new Vector(0, 0, 0);
+
+class ClusterPoint {
+
+    constructor(height, block_id, margin, info) {
+        this.height = height;
+        this.block_id = block_id;
+        this.margin = margin;
+        this.info = info;
+    }
+
+}
 
 // ChunkCluster
 export class ChunkCluster {
@@ -38,18 +50,37 @@ export class ChunkCluster {
             let x = Math.round(this.randoms.double() * 64) + CLUSTER_PADDING;
             let z = Math.round(this.randoms.double() * 64) + CLUSTER_PADDING;
             let w = Math.round(this.randoms.double() * (64 - CLUSTER_PADDING));
-            x = Math.ceil(x / 12) * 12;
-            z = Math.ceil(z / 12) * 12;
-            w = Math.ceil(w / 12) * 12;
+            x = Math.ceil(x / STREET_WIDTH) * STREET_WIDTH;
+            z = Math.ceil(z / STREET_WIDTH) * STREET_WIDTH;
+            w = Math.ceil(w / STREET_WIDTH) * STREET_WIDTH;
             if(this.randoms.double() < .5) {
+                // horizontal
                 for(let i = 0; i < w; i++) {
-                    this.mask[z * CLUSTER_SIZE.x + (x + i)] = new Color(8, 0, 1);
-                    this.mask[(z + 1) * CLUSTER_SIZE.x + (x + i)] = new Color(8, 0, 1);
+                    this.mask[z * CLUSTER_SIZE.x + (x + i)] = new ClusterPoint(1, 468, 5, null);
+                    this.mask[(z + 1) * CLUSTER_SIZE.x + (x + i)] = new ClusterPoint(1, 468, 5, null);
                 }
+                this.addBuilding(x + 3, z + 3, 11, 11);
             } else {
+                // vertical
                 for(let i = z; i < z + w; i++) {
-                    this.mask[i * CLUSTER_SIZE.x + x] = new Color(8, 0, 1);
-                    this.mask[i * CLUSTER_SIZE.x + (x + 1)] = new Color(8, 0, 1);
+                    this.mask[i * CLUSTER_SIZE.x + x] = new ClusterPoint(1, 468, 5, null);
+                    this.mask[i * CLUSTER_SIZE.x + (x + 1)] = new ClusterPoint(1, 468, 5, null);
+                }
+                this.addBuilding(x + 3, z + 3, 11, 11);
+            }
+        }
+    }
+
+    // addBuilding
+    addBuilding(dx, dz, width, depth) {
+        for(let i = 0; i < width; i++) {
+            for(let j = 0; j < depth; j++) {
+                const x = dx + i;
+                const z = dz + j;
+                if(i == 0 || j == 0 || i == width - 1 || j == depth - 1) {
+                    this.mask[z * CLUSTER_SIZE.x + x] = new ClusterPoint(1, 546, 1, null);
+                } else {
+                    this.mask[z * CLUSTER_SIZE.x + x] = new ClusterPoint(8, 98, 5, null);
                 }
             }
         }
@@ -85,18 +116,18 @@ export class ChunkCluster {
             for(let j = 0; j < CHUNK_SIZE_Z; j++) {
                 let x = start_x + i;
                 let z = start_z + j;
-                let info = this.mask[z * CLUSTER_SIZE.x + x];
-                if(info && info.r) {
+                let point = this.mask[z * CLUSTER_SIZE.x + x];
+                if(point && point.height) {
                     const cell = map.info.cells[i][j];
                     if(cell.biome.code == 'OCEAN') {
                         continue;
                     }
-                    for(let k = 0; k < info.r; k++) {
+                    for(let k = 0; k < point.height; k++) {
                         let y = cell.value2 + k - CHUNK_Y_BOTTOM - 1;
-                        let block_id = getBlock(i, y, j);
-                        if(block_id && (block_id == cell.biome.dirt_block)) {
-                            setBlock(i, y, j, 468, null);
-                        }
+                        //let block_id = getBlock(i, y, j);
+                        //if(block_id && (block_id == cell.biome.dirt_block)) {
+                        setBlock(i, y, j, point.block_id, null);
+                        //}
                     }
                 }
             }
@@ -114,7 +145,7 @@ export class ChunkCluster {
                 const dz = z + j;
                 if(dx >= 0 && dz >= 0 && dx < CLUSTER_SIZE.x && z < CLUSTER_SIZE.z) {
                     const info = this.mask[dz * CLUSTER_SIZE.x + dx];
-                    if(info && info.r) {
+                    if(info && info.height) {
                         return true;
                     }
                 }
