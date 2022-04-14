c372777c5877f8893d69e9be46d64377b484df13
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2022-04-14 22:40:02 +0300

    start regions

--- www/js/light/CubeTexturePool.js
+++ www/js/light/CubeTexturePool.js
@@ -1,5 +1,24 @@
-export class BigCubeTexture {
-    constructor() {
+import {Vector} from '../helpers.js';
+import {BaseTexture3D} from "../renders/BaseTexture3D.js";
+
+export class GridCubeTexture {
+    constructor({ textureOptions, dims }) {
+        this.textureOptions = textureOptions;
+        this.dims = dims;
+        this.freeRegions = []
+        this.allocatedRegions = []
+    }
+
+    init() {
+        const { dims, textureOptions } = this;
+
+        this.baseTexture = new BaseTexture3D(textureOptions);
+
+        for (let i = 0; i < dims.x; i++)
+            for (let j = 0; j < dims.y; j++)
+                for (let k = 0; k < dims.z; k++) {
+
+                }
     }
 
     allocate() {
@@ -11,27 +30,69 @@ export class CubeTexturePool {
         defWidth = 16,
         defHeight = 16,
         defDepth = 40,
+        bigWidth = 128,
+        bigHeight = 128,
+        bigDepth = 128,
+        type = 'rgba8unorm',
+        filter = 'linear',
         renderer
     }) {
-        this.defWidth = defWidth;
-        this.defHeight = defHeight;
-        this.defDepth = defDepth;
+        this.bigWidth = bigWidth;
+        this.bigHeight = bigHeight;
+        this.bigDepth = bigDepth;
+        this.defTex = {
+            width: defWidth,
+            height: defHeight,
+            depth: defDepth,
+            filter, type,
+            context: renderer
+        }
         this.singles = []
         this.pools = []
         this.renderer = renderer
+        this.currentPoolIndex = 0
     }
 
     alloc({width, height, depth, type, filter, data}) {
-        // if (width !== this.width || height !== this.defHeight || depth !== this.defDepth) {
+        const { defTex } = this;
+        // if (width !== this.defTex.width || height !== this.defTex.height || depth !== this.defTex.depth) {
             // create a single
-        const tex = this.renderer.createTexture3D(
-            {width, height, depth, type, filter, data})
-        tex.ownerPool = this;
-        this.singles.push(tex);
-        return tex;
+            const tex = this.renderer.createTexture3D(
+                {width, height, depth, type, filter, data})
+            tex.ownerPool = this;
+            this.singles.push(tex);
+            return tex;
+        // } else {
+            // 1. choose pool
+
         // }
     }
 
+    findFreeRegion() {
+        let cur = this.currentPoolIndex;
+        let repeat = 0;
+        let selected = null;
+        while (repeat < this.pools.length) {
+            if (this.pools[cur].freeRegions.length > 0) {
+                selected = this.pools[cur];
+                break;
+            }
+            cur = (cur + 1) % this.pools.length
+            repeat++
+        }
+        if (!selected) {
+            let newCube = new GridCubeTexture(
+                {
+                    textureOptions: this.defTex,
+                    dims: new Vector(
+                        Math.floor(this.bigWidth / this.defWidth),
+                        Math.floor(this.bigHeight / this.defHeight),
+                        Math.floor(this.bigDepth / this.defDepth),
+                    )
+                }
+            )
+        }
+    }
 
     dealloc(tex) {
         // if (tex.isRegion) {
