714bb06b8dedf65f3ec4859fafe297fa9b15b480
sciner <sciner2008@gmail.com>
2022-04-17 20:40:02 +0400

    cluster debugger

--- www/tools/cluster/generator.js
+++ www/tools/cluster/generator.js
@@ -1,9 +1,16 @@
 import {BLOCK} from '../../js/blocks.js';
 // await BLOCK.init({texture_pack: 'base'});
 
-BLOCK.COBBLESTONE = {id: 1};
-BLOCK.COBBLESTONE_WALL = {id: 2};
-BLOCK.OAK_FENCE = {id: 3};
+const blocks = [];
+blocks.push({id: 2, name: 'COBBLESTONE', color: '#c0c0c0ff'});
+blocks.push({id: 8, name: 'COBBLESTONE_WALL', color: '#a0a0a0ff'});
+blocks.push({id: 468, name: 'OAK_FENCE', color: '#a39565ff'});
+
+const colors = new Map();
+for(let b of blocks) {
+    BLOCK[b.name] = b;
+    colors.set(b.id, b.color);
+}
 
 import {Vector} from '../../js/helpers.js';
 import {ClusterVilage} from '../../js/terrain_generator/cluster/vilage.js';
@@ -20,28 +27,26 @@ class Sandbox {
         this.draw();
     }
 
+    // Распечатка канваса
     draw() {
-        // Распечатка канваса
-        // const cnv = document.getElementById('sandbox_canvas');
-        // const ctx = cnv.getContext('2d');
         const scale = 4;
-        ctx.fillStyle = "#FFFFFF";
+        ctx.fillStyle = "#22aa00";
         ctx.fillRect(0, 0, this.settings.size * scale, this.settings.size * scale);
         for(var x = 0; x < this.settings.size; x++) {
             for(var z = 0; z < this.settings.size; z++) {
                 const point = this.cluster.mask[z * this.settings.size + x];
                 if(point) {
-                    const cell = point.block_id;
-                    if(cell === 8) {
-                        ctx.fillStyle = "#000000";
-                    } else if(cell === 2) {
-                        ctx.fillStyle = "#FF000088";
+                    const block_id = Array.isArray(point.block_id) ? point.block_id[point.block_id.length - 1] : point.block_id;
+                    let c = colors.get(block_id);
+                    ctx.fillStyle = c;
+                    if(!c) {
+                        console.log(block_id, colors.get(block_id));
                     }
                 } else {
                     if((x + z) % 2 == 0) {
                         continue;
                     }
-                    ctx.fillStyle = "#00000022";
+                    ctx.fillStyle = "#00000011";
                 }
                 ctx.fillRect(x * scale, z * scale, 1 * scale, 1 * scale);
             }
@@ -52,8 +57,8 @@ class Sandbox {
         for(let b of this.cluster.schema.house_list.values()) {
             ctx.fillStyle = "#0000ff55";
             ctx.fillRect(b.x * scale, b.z * scale, b.width * scale, b.height * scale);
-            ctx.fillStyle = "#fff";
-            ctx.fillText(`${b.x}x${b.z}`, (b.x + 1) * scale, (b.z + 1) * scale);
+            // ctx.fillStyle = "#fff";
+            // ctx.fillText(`${b.x}x${b.z}`, (b.x + 1) * scale, (b.z + 1) * scale);
         }
     }
 
