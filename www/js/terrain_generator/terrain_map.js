3dd13a8e6e366367fc58102237b348bc5ccb3e30
sciner <sciner2008@gmail.com>
2022-04-29 17:56:14 +0400

    terrain_map

--- www/js/terrain_generator/terrain_map.js
+++ www/js/terrain_generator/terrain_map.js
@@ -82,77 +82,79 @@ export class TerrainMap {
         return maps;
     }
 
+    //
+    makePoint(px, pz, cluster_max_height) {
+        const noisefn = this.noisefn;
+        const H = 68;
+        const HW = 64;
+        // Влажность
+        let humidity = Helpers.clamp((noisefn(px / GENERATOR_OPTIONS.SCALE_HUMIDITY, pz / GENERATOR_OPTIONS.SCALE_HUMIDITY) + 0.5) / 2, 0, 1);
+        // Экватор
+        let equator = Helpers.clamp((noisefn(px / GENERATOR_OPTIONS.SCALE_EQUATOR, pz / GENERATOR_OPTIONS.SCALE_EQUATOR) + 0.8), 0, 1);
+        // Высота горы в точке
+        const octave1 = noisefn(px / 20, pz / 20);
+        let value = noisefn(px / 150, pz / 150, 0) * .4 + 
+            noisefn(px / 1650, pz / 1650) * .1 +
+            noisefn(px / 650, pz / 650) * .25 +
+            octave1 * .05 +
+            noisefn(px / 350, pz / 350) * .5 +
+            noisefn(px / 25, pz / 25) * (0.01568627 * octave1);
+        // Get biome
+        let biome = BIOMES.getBiome((value * HW + H) / 255, humidity, equator);
+        if(biome.code == 'OCEAN' || biome.code == 'BEACH') {
+            value = value * biome.max_height + H;
+        } else {
+            // smooth with clusters
+            value = value * (cluster_max_height ? Math.min(cluster_max_height - 1, (cluster_max_height + biome.max_height) / 2) : biome.max_height) + H;
+        }
+        value = parseInt(value);
+        value = Helpers.clamp(value, 4, 2500);
+        biome = BIOMES.getBiome(value / 255, humidity, equator);
+        // Pow
+        let diff = value - GENERATOR_OPTIONS.WATER_LINE;
+        if(diff < 0) {
+            value -= (GENERATOR_OPTIONS.WATER_LINE - value) * .65 - 1.5;
+        } else {
+            value = GENERATOR_OPTIONS.WATER_LINE + Math.pow(diff, 1 + diff / HW);
+        }
+        value = parseInt(value);
+        return {value, biome, humidity, equator};
+    }
+
     // generateMap
     generateMap(chunk, noisefn) {
         let cached = this.maps_cache.get(chunk.addr);
         if(cached) {
             return cached;
         }
-        const options               = GENERATOR_OPTIONS;
         // Result map
-        let map                     = new Map(chunk, options);
-        //
-        const SX                    = chunk.coord.x;
-        const SZ                    = chunk.coord.z;
+        const map                   = new Map(chunk, GENERATOR_OPTIONS);
         const cluster               = ClusterManager.getForCoord(chunk.coord);
-        const H                     = 68;
         //
         for(let x = 0; x < chunk.size.x; x++) {
             for(let z = 0; z < chunk.size.z; z++) {
-                let px = SX + x;
-                let pz = SZ + z;
+                let px = chunk.coord.x + x;
+                let pz = chunk.coord.z + z;
                 let cluster_max_height = null;
                 if(!cluster.is_empty && cluster.cellIsOccupied(px, 0, pz, MAP_CLUSTER_MARGIN)) {
                     cluster_max_height = cluster.max_height;
                 }
-                // Высота горы в точке
-                let value = noisefn(px / 150, pz / 150, 0) * .4 + 
-                    noisefn(px / 1650, pz / 1650) * .1 + // 10 | 1650
-                    noisefn(px / 650, pz / 650) * .25 + // 65 | 650
-                    noisefn(px / 20, pz / 20) * .05 +
-                    noisefn(px / 350, pz / 350) * .5;
-                value += noisefn(px / 25, pz / 25) * (4 / 255 * noisefn(px / 20, pz / 20));
-                // Влажность
-                let humidity = Helpers.clamp((noisefn(px / options.SCALE_HUMIDITY, pz / options.SCALE_HUMIDITY) + 0.5) / 2, 0, 1);
-                // Экватор
-                let equator = Helpers.clamp((noisefn(px / options.SCALE_EQUATOR, pz / options.SCALE_EQUATOR) + 0.8), 0, 1);
-                // Get biome
-                let biome = BIOMES.getBiome((value * 64 + H) / 255, humidity, equator);
-                if(biome.code == 'OCEAN' || biome.code == 'BEACH') {
-                    value = value * biome.max_height + H;
-                } else {
-                    value = value * (cluster_max_height ? Math.min(cluster_max_height - 1, (cluster_max_height + biome.max_height) / 2) : biome.max_height) + H;
-                }
-                value = parseInt(value);
-                value = Helpers.clamp(value, 4, 2500);
-                biome = BIOMES.getBiome(value / 255, humidity, equator);
-                // Pow
-                let diff = value - options.WATER_LINE;
-                if(diff < 0) {
-                    value -= (options.WATER_LINE - value) * .65 - 1.5;
-                } else {
-                    value = options.WATER_LINE + Math.pow(diff, 1 + diff / 64);
-                }
-                value = parseInt(value);
+                const {value, biome, humidity, equator} = this.makePoint(px, pz, cluster_max_height);
                 // Different dirt blocks
-                let ns = noisefn(px / 5, pz / 5);
-                let index = parseInt(biome.dirt_block.length * Helpers.clamp(Math.abs(ns + .3), 0, .999));
-                let dirt_block_id = biome.dirt_block[index];
-                // Create map cell
-                let cell = new MapCell(value, humidity, equator, biome, dirt_block_id);
-                //if(biome.code == 'OCEAN') {
-                //    cell.dirt_block_id = BLOCK.STILL_WATER.id;
-                //}
-                try {
-                    map.cells[x][z] = cell;
-                } catch(e) {
-                    debugger;
+                let dirt_block_id = biome.dirt_block[0];
+                if(biome.dirt_block.length > 1) {
+                    const ns = noisefn(px / 5, pz / 5);
+                    const index = parseInt(biome.dirt_block.length * Helpers.clamp(Math.abs(ns + .3), 0, .999));
+                    dirt_block_id = biome.dirt_block[index];
                 }
+                // Create map cell
+                map.cells[x][z] = new MapCell(value, humidity, equator, biome, dirt_block_id);
             }
         }
         // Clear maps_cache
-        this.maps_cache.reduce(20000);
-        return this.maps_cache.add(chunk.addr, map);
+        // this.maps_cache.reduce(20000);
+        this.maps_cache.set(chunk.addr, map);
+        return map;
     }
 
 }
