6a1db03a345046935d76f5c5b8bb4fa878820393
kr0n4ik <kr0n4ik@hotmail.com>
2022-06-02 20:01:18 +0300

    add pig
    add pig

--- node_server/fsm/brain/pig.js
+++ node_server/fsm/brain/pig.js
@@ -1,6 +1,7 @@
 import {FSMBrain} from "../brain.js";
-
-import {Vector} from "../../../www/js/helpers.js";
+import { BLOCK } from "../../../www/js/blocks.js";
+import { Vector } from "../../../www/js/helpers.js";
+import { PickatActions } from "../../../www/js/block_action.js";
 
 export class Brain extends FSMBrain {
 
@@ -15,9 +16,78 @@ export class Brain extends FSMBrain {
             stepHeight: 1,
             playerHalfWidth: .5
         });
-        this.isAggrressor = false;
+
+        this.live = 10;
+        this.follow_distance = 16;
+
         // Начинаем с просто "Стоять"
         this.stack.pushState(this.doStand);
+    }
+
+    findTarget() {
+        if (this.target == null) {
+            const mob = this.mob;
+            const players = this.getPlayersNear(mob.pos, this.follow_distance, false);
+            let friends = [];
+            for (let player of players) {
+                if (player.state.hands.right.id == BLOCK.CARROT.id) {
+                    friends.push(player);
+                }
+            }
+            if (friends.length > 0) {
+                const rnd = (Math.random() * friends.length) | 0;
+                const player = friends[rnd];
+                this.target = player.session.user_id;
+                this.stack.replaceState(this.doCatch);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    async doCatch(delta) {
+        const mob = this.mob;
+        const player = mob.getWorld().players.get(this.target);
+        if (!player || player.state.hands.right.id != BLOCK.CARROT.id || player.game_mode.isSpectator()) {
+            this.target = null;
+            this.isStand(1.0);
+            this.sendState();
+            return;
+        }
+
+        if (Math.random() < 0.5) {
+            this.mob.rotate.z = this.angleTo(player.state.pos);
+        }
+
+        const forward = (mob.pos.distance(player.state.pos) > 1.5) ? true : false;
+
+        this.updateControl({
+            yaw: this.mob.rotate.z,
+            forward: forward,
+            jump: this.checkInWater()
+        });
+
+        this.applyControl(delta);
+        this.sendState();
+    }
+
+
+    async onKill(owner, type) {
+        const mob = this.mob;
+        const world = mob.getWorld();
+        if (owner != null) {
+            const actions = new PickatActions();
+            const rnd_count_porkchop = ((Math.random() * 2) | 0) + 1;
+
+            let items = { pos: mob.pos, items: [] };
+            items.items.push({ id: BLOCK.PORKCHOP.id, count: rnd_count_porkchop });
+
+            actions.addDropItem(items);
+
+            actions.addPlaySound({ tag: 'madcraft:block.pig', action: 'hurt', pos: mob.pos.clone() }); //Звук смерти
+
+            await world.applyActions(owner, actions);
+        }
     }
 
 }
