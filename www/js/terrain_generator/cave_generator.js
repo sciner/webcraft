497e381b4dd1221dd16d6135b1592b667212ccec
sciner <sciner2008@gmail.com>
2022-06-17 22:01:06 +0400

    caves

--- www/js/terrain_generator/cave_generator.js
+++ www/js/terrain_generator/cave_generator.js
@@ -1,13 +1,11 @@
 import { CHUNK_SIZE_X, CHUNK_SIZE_Z } from "../chunk_const.js";
 import { Vector } from "../helpers.js";
 
-const CAVE_SCALE            = 1;
-const CAVE_NOISE_SCALE      = 4.5;
-const CAVE_WIDTH            = 0.08 * CAVE_SCALE;
-const CAVE_OCTAVE_1         = 64 / CAVE_SCALE;
-const CAVE_OCTAVE_2         = CAVE_OCTAVE_1 / CAVE_NOISE_SCALE;
-const LAYER_SHIFT           = 8192;
-const CAVE_LAYERS           = 3;
+const CAVE_LAYERS = [
+                        {y: 56, octave1: 28.4, octave2: 28.4, width: 0.12, height: 24, shift: 64000},
+                        {y: 32, octave1: 32, octave2: 7.11, width: 0.12, height: 48, shift: 48000},
+                        {y: 0, octave1: 32, octave2: 7.11, width: 0.12, height: 16, shift: 16000},
+                    ];
 
 export class CaveGenerator {
 
@@ -16,24 +14,25 @@ export class CaveGenerator {
         this.chunk_coord = new Vector(chunk_coord.x, 0, chunk_coord.z);
         this.layers = [];
 
-        for(let i = 0; i < CAVE_LAYERS; i++) {
+        for(let i = 0; i < CAVE_LAYERS.length; i++) {
+            const options = CAVE_LAYERS[i];
             const layer = new Array(CHUNK_SIZE_X * CHUNK_SIZE_Z);
             for(let x = 0; x < CHUNK_SIZE_X; x++) {
                 for(let z = 0; z < CHUNK_SIZE_Z; z++) {
-                    const ax = chunk_coord.x + x + LAYER_SHIFT * i;
-                    const az = chunk_coord.z + z + LAYER_SHIFT * i;
-                    const value1 = noisefn(ax / CAVE_OCTAVE_1, az / CAVE_OCTAVE_1);
-                    const value2 = noisefn(ax / CAVE_OCTAVE_2, az / CAVE_OCTAVE_2);
+                    const ax = chunk_coord.x + x + options.shift;
+                    const az = chunk_coord.z + z + options.shift;
+                    const value1 = noisefn(ax / options.octave1, az / options.octave1);
+                    const value2 = noisefn(ax / options.octave2, az / options.octave2);
                     // calc value
                     let value = value1 * .8 + value2 * .1;
                     if(value < 0) value *= -1;
-                    if(value > CAVE_WIDTH) continue;
-                    value = 1 - value / CAVE_WIDTH;
+                    if(value > options.width) continue;
+                    value = 1 - value / options.width;
                     // generate vertical position of worm
-                    const y = (i * 32) + Math.sin(noisefn(ax / 128, az / 128)) * 32 + (i * 3);
+                    const y = options.y + Math.sin(noisefn(ax / 128, az / 128)) * options.height;
                     layer[z * CHUNK_SIZE_X + x] = {
-                        value, // density
-                        y // vertical position
+                        density: value / 1.5, // density
+                        y: y // vertical position
                     };
                 }
             }
@@ -46,7 +45,7 @@ export class CaveGenerator {
     getPoint(xyz, map_cell, in_ocean) {
         const x = xyz.x - this.chunk_coord.x;
         const z = xyz.z - this.chunk_coord.z;
-        for(let i = 0; i < CAVE_LAYERS; i++) {
+        for(let i = 0; i < CAVE_LAYERS.length; i++) {
             const layer = this.layers[i];
             const cell = layer[z * CHUNK_SIZE_X + x];
             if(!cell) {
@@ -58,7 +57,7 @@ export class CaveGenerator {
                 }
             }
             const dist = xyz.y - cell.y;
-            const dens = cell.value / 1.5
+            const dens = cell.density
             if(dist < -5 * dens || dist > 5 * dens) {
                 continue;
             }
