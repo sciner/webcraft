1eb5cccb885e1d72a9793ff540fe90faa34bd8a2
Konstantin Timoshenko <rondo.devil@gmail.com>
2022-06-28 00:55:09 +0300

    feat: fix msdf text fill

--- www/js/block_style/text.js
+++ www/js/block_style/text.js
@@ -40,64 +40,87 @@ export default class style {
         };
     }
 
-    // Build function
-    static func(block, vertices, chunk, x, y, z, neighbours, biome, dirt_color, unknown, matrix = null, pivot = null, force_tex) {
+    static fillRun ({
+        aabb,
+        chars,
+        vertices,
+        baseHeight = 38,
+        lines = 4,
+        pivot,
+        matrix,
+        center,
+        alignCenter = false,
+    }) {
+        const aabbc = style._aabbc;
+        const totalHeight = baseHeight * lines;
 
-        if(!block.extra_data || !block.extra_data.aabb) {
-            return;
+        let cursorX = 0;
+        let cursorY = 1;
+        let maxX = 0;
+        let maxY = 0;
+
+        // pre-pass
+        // compute real width
+        for(let char of chars) {
+            if (char.char === '\r') {
+                cursorY ++;
+                cursorX = 0;
+                continue;
+            }
+
+            cursorX += char.uv.xadvance;
+            maxX = Math.max(maxX, cursorX);
         }
 
-        const LETTER_PER_LINE       = 20;
-        const LINES                 = 4;
+        maxY = cursorY * baseHeight;
 
-        const aabb                  = style._aabb;
-        const aabbc                 = style._aabbc;
-        const center                = style._center.set(x, y, z);
-        aabb.copyFrom(block.extra_data.aabb).pad(.1 / 16);
+        const refScale = Math.min( aabb.height / totalHeight, aabb.width / maxX);
 
-        const LETTER_W              = aabb.width / (LETTER_PER_LINE + 2);
-        const LETTER_H              = aabb.height / (LINES + 2);
-        const LETTER_SPACING_MUL    = 1;
-        const PADDING               = style._padding.set(LETTER_W, -LETTER_H / 2, 0);
-
-        // Letter position
-        let cx                      = 0;
-        let cy                      = 0;
-
-        function wrap() {
-            cx = 0;
-            cy++;
-        }
-
-        // Each over all text chars
-        /**
-         * @type {Char[]}
-         */
-        const chars = block.extra_data.chars;
+        cursorX = 0;
+        cursorY = 0;
 
         for(let char of chars) {
             if(char.char == "\r") {
-                wrap();
+                cursorY ++
+                cursorX = 0;
                 continue;
             }
+
+            const uv = char.uv;
             // Letter texture
-            let c = [
+            const c = [
                 char.xn + char.width / 2,
                 char.yn + char.height / 2,
                 char.width,
                 char.height
             ];
+
             // Letter position
             aabbc.copyFrom(aabb);
-            aabbc.x_min += (cx * LETTER_W) * LETTER_SPACING_MUL;
-            aabbc.x_max = aabbc.x_min + LETTER_W;
-            aabbc.y_min = aabbc.y_max - (cy+1) * LETTER_H;
-            aabbc.y_max = aabbc.y_min + LETTER_H;
+
+            aabbc.x_min += cursorX * refScale * uv.xadvance;
+            aabbc.x_max = aabbc.x_min + refScale * uv.width;
+
+            aabbc.y_min += refScale * (-cursorY * baseHeight - uv.yoffset);
+            aabbc.y_max = aabbc.y_min + refScale * uv.height;
+
+            cursorX++;
+
             aabbc.translate(
-                PADDING.x - aabbc.width * char.shift_x,
-                PADDING.y - aabbc.height * char.shift_y,
-                PADDING.z
+                0, 
+                - aabbc.height + aabb.height,
+                0
             );
+
+            if (alignCenter) {
+                aabbc.translate(
+                    -(maxX * refScale - aabb.width) * 0.5, 
+                    (maxY * refScale - aabb.height) * 0.5,
+                    0
+                );
+
+            }
+
             // Push letter vertices
             pushAABB(
                 vertices,
@@ -109,48 +132,61 @@ export default class style {
                 },
                 center
             );
-            cx++;
+        }
+    }
+    // Build function
+    static func(block, vertices, chunk, x, y, z, neighbours, biome, dirt_color, unknown, matrix = null, pivot = null, force_tex) {
+
+        if(!block.extra_data || !block.extra_data.aabb) {
+            return;
         }
 
-        // Draw signature and date on backside
+        const BASE_HEIGHT           = 38; // from atlas
+        const LINES                 = 4;
+
+        const aabb                  = style._aabb;
+        const aabbc                 = style._aabbc;
+        const center                = style._center.set(x, y, z);
+
+        aabb.copyFrom(block.extra_data.aabb).pad(.1 / 16);
+        aabb.x_min += 1 / 32;
+        aabb.y_min += 1 / 16;
+        aabb.x_max -= 1 / 32;
+        aabb.y_max -= 1 / 32;
+
+
+        // Each over all text chars
+        /**
+         * @type {Char[]}
+         */
+        const chars = block.extra_data.chars;
+
+        const args = {
+            aabb, 
+            chars, 
+            vertices, 
+            lines: LINES + 1, 
+            baseHeight: BASE_HEIGHT,
+            center,
+            matrix,
+            pivot
+        }
+        style.fillRun(args);
+
         const sign = block.extra_data.sign;
         if(sign) {
-            cx = 0;
-            // cy = 10;
-            const SCALE_SIGN = 2;
-            const plus_x = aabb.width * .5 - (sign.length * (LETTER_W * (LETTER_SPACING_MUL / SCALE_SIGN))) / 2;
-            for(let char of sign) {
-                if(char.char == "\r") {
-                    wrap();
-                    continue;
-                }
-                // Letter texture
-                let c = [
-                    char.xn + char.width / 2,
-                    char.yn + char.height / 2,
-                    char.width,
-                    char.height
-                ];
-                // Letter position
-                aabbc.copyFrom(aabb);
-                aabbc.x_min += (cx * LETTER_W) * (LETTER_SPACING_MUL / SCALE_SIGN);
-                aabbc.x_max = aabbc.x_min + LETTER_W / SCALE_SIGN;
-                aabbc.y_min = aabb.y_min + LETTER_H / SCALE_SIGN, // aabbc.y_max - (cy+1) * LETTER_H / SCALE_SIGN;
-                aabbc.y_max = aabbc.y_min + LETTER_H / SCALE_SIGN; // + LETTER_H / SCALE_SIGN;
-                aabbc.translate(PADDING.x + plus_x, 0, PADDING.z);
-                // Push letter vertices
-                pushAABB(
-                    vertices,
-                    aabbc,
-                    pivot,
-                    matrix,
-                    {
-                        south:  new AABBSideParams(c, QUAD_FLAGS.QUAD_FLAG_SDF, 1, null, null, false)
-                    },
-                    center
-                );
-                cx++;
-            }
+            aabb.y_min -= 1/24;
+            aabb.y_max = aabb.y_min + aabb.height * 0.1;
+
+            style.fillRun({
+                ...args,
+                aabb,
+                // render only 1 line
+                // at center
+                chars: sign,
+                lines: 1,
+                alignCenter: true,
+            })
         }
 
         return null;
