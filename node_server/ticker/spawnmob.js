1bd0bcd4535e97beb622fc7768f8e0d56fda1e3e
kr0n4ik <kr0n4ik@hotmail.com>
2022-07-11 17:38:36 +0300

    test spawn zombie
    test spawn zombie

--- node_server/ticker/spawnmob.js
+++ node_server/ticker/spawnmob.js
@@ -12,26 +12,60 @@ export default class Ticker {
         const ticking = v.ticking;
         const extra_data = tblock.extra_data;
         const updated_blocks = [];
+        
         if(v.ticks % extra_data.max_ticks == 0) {
-            const spawn_pos = v.pos.clone().addSelf(new Vector(.5, 0, .5));
-            const params = {
-                type           : extra_data.type,
-                skin           : extra_data.skin,
-                pos            : spawn_pos,
-                pos_spawn      : spawn_pos.clone(),
-                rotate         : new Vector(0, 0, 0).toAngles()
-            };
-            // Spawn mob
-            console.log('Spawn mob', v.pos.toHash());
-            await world.mobs.create(params);
-            updated_blocks.push({pos: v.pos.clone(), item: {id: BLOCK.AIR.id}, action_id: ServerClient.BLOCK_ACTION_MODIFY});
-            // Delete completed block from tickings
-            this.delete(v.pos);
+            extra_data.max_ticks = Math.random() * 600 | 0 + 200;
+            const pos = v.pos.clone();
+            console.log(extra_data.max_ticks);
+            
+             //Проврерям наличие игроков в радиусе 16 блоков
+            const players = world.getPlayersNear(pos, 16, true);
+            if (players.length == 0) {
+                return;
+            }
+            
+            //Проврерям количество мобов в радиусе(в радиусе 4 блоков не должно быть больше 5 мобов)
+            const mobs = world.getMobsNear(pos, 9);
+            if (mobs.length >= 6) {
+                return;
+            }
+            
+            //Место спауна моба, 4 попытки. Если на координатак моб, игрок или блок, то не спауним
+            for (let i = 0; i < 4; i++) {
+                const x = (Math.random() * 4 | 0) - (Math.random() * 4 | 0);
+                const z = (Math.random() * 4 | 0) - (Math.random() * 4 | 0);
+                const y = Math.random() * 2 | 0;
+                const spawn_pos = pos.addSelf(new Vector(x, y, z)).flooredSelf(); //@todo Вроде как не обязательно округление
+                for (let player of players) {
+                    if (player.state.pos.x == spawn_pos.x && player.state.pos.z == spawn_pos.z) {
+                        return;
+                    }
+                }
+                
+                for (let mob of mobs) {
+                    if (mob.pos.x == spawn_pos.x && mob.pos.z == spawn_pos.z) {
+                        return;
+                    }
+                }
+                //Проверяем есть ли блок на пути и что под ногами для нейтральных мобов
+                const body = world.getBlock(spawn_pos);
+                const legs = world.getBlock(spawn_pos.sub(Vector.YP));
+                if (body.id != 0) {
+                    return;
+                }
+                
+                 const params = {
+                    type           : extra_data.type,
+                    skin           : extra_data.skin,
+                    pos            : spawn_pos,
+                    pos_spawn      : spawn_pos.clone(),
+                    rotate         : new Vector(0, 0, 0).toAngles()
+                };
+                
+                console.log('Spawn mob', pos.toHash());
+                await world.mobs.create(params);
+            }
         }
-        if(!extra_data) {
-            return;
-        }
-        return updated_blocks;
     }
 
 }
