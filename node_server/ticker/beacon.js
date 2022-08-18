4691bc75ad95e3f5faff80ee59e0cc37f5827b54
sciner <sciner2008@gmail.com>
2022-08-19 03:02:48 +0400

    optimize speed of beacon ticker

--- node_server/ticker/beacon.js
+++ node_server/ticker/beacon.js
@@ -1,6 +1,8 @@
 import { BLOCK } from '../../www/js/blocks.js';
-import { Vector, DIRECTION } from '../../www/js/helpers.js';
+import { Vector } from '../../www/js/helpers.js';
 import { ServerClient } from '../../www/js/server_client.js';
+
+const CHECK_MAX_BLOCKS_OVER_ME = 310;
 
 export default class Ticker {
 
@@ -8,14 +10,18 @@ export default class Ticker {
     
     //
     static func(world, chunk, v) {
+
         const tblock = v.tblock;
         const ticking = v.ticking;
         const extra_data = tblock.extra_data;
         const pos = v.pos.clone();
+
+        // only every ~4 sec
         if(v.ticks % 40 != 0) {
             return;
         }
         
+        // драгоценный блок или нет
         const isPrecious = (n) => {
             for (let i = -n; i <= n; i++) {
                 for (let j = -n; j <= n; j++) {
@@ -28,6 +34,7 @@ export default class Ticker {
             }
             return true;
         };
+
         // старые данные
         const level = extra_data.level;
         // проверяем бонусы и включение
@@ -48,8 +55,10 @@ export default class Ticker {
         
         // проверка преград на пути луча или стекла для раскраски
         if (extra_data.level > 0) {
-            for (let i = 1; i < 310; i++) {
-                const block = world.getBlock(pos.add(new Vector(0, i, 0)));
+            const check_vec = pos.clone();
+            for(let i = 1; i < CHECK_MAX_BLOCKS_OVER_ME; i++) {
+                check_vec.y++;
+                const block = world.getBlock(check_vec);
                 if (block && block.id != BLOCK.AIR.id) {
                     extra_data.level = 0;
                     break;
@@ -58,7 +67,7 @@ export default class Ticker {
         }
         
         const updated_blocks = [];
-        if (level != extra_data.level) {
+        if(level != extra_data.level) {
             updated_blocks.push({pos: pos, item: tblock.convertToDBItem(), action_id: ServerClient.BLOCK_ACTION_MODIFY});
         }
         return updated_blocks;
