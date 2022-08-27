f42d2dab94e8ac3329d61843a7bfb2f530b73d56
sciner <sciner2008@gmail.com>
2022-08-27 23:41:10 +0400

    remove unused style files

--- node_server/index.js
+++ node_server/index.js
@@ -113,7 +113,6 @@ app.use(compression({
 
 // Serves resources from public folder
 app.use('/style', expressLess(__dirname + '/../www/style', { compress: true, debug: true }));
-app.use('/src/style', expressLess(__dirname + '/../www/src/style', { compress: true }));
 app.use(express.static('../www/'));
 
 // API
