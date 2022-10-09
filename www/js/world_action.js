389edfe9815a07e83cd4058f88610731de47dff6
sciner <sciner2008@gmail.com>
2022-10-10 02:58:32 +0400

    f

--- www/js/world_action.js
+++ www/js/world_action.js
@@ -2168,8 +2168,8 @@ async function setFurnitureUpholstery(e, world, pos, player, world_block, world_
                 action_id: ServerClient.BLOCK_ACTION_MODIFY
             }]);
             actions.addPlaySound({tag: 'madcraft:block.cloth', action: 'hit', pos: new Vector(pos), except_players: [player.session.user_id]});
+            return true;
         }
-        return true;
     }
     return false;
 }
