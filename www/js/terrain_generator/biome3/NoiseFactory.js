b8f6699ef38b07feae55d1812336cc39e1d485fc
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2022-11-27 17:07:55 +0400

    WIP

---
+++ www/js/terrain_generator/biome3/NoiseFactory.js
@@ -0,0 +1,13 @@
+export class MyNoise3d {
+    constructor() {
+        this.outputSize = null;
+    }
+
+    async init(outputSize, seed) {
+        this.outputSize = outputSize;
+    }
+
+    async gen() {
+
+    }
+}
