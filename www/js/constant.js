991802c4492e6830eac41abf14dcd4ec1c9f1273
sciner <sciner2008@gmail.com>
2022-12-13 20:45:57 +0400

    tx_cnt64

--- www/js/constant.js
+++ www/js/constant.js
@@ -10,6 +10,7 @@ export const NO_TICK_BLOCKS                 = false;
 export const BODY_ROTATE_SPEED              = 7;
 export const HEAD_MAX_ROTATE_ANGLE          = 45; // in degree
 export const GRASS_PALETTE_OFFSET           = 256; // (in px) offset in mask_color.png for grass palette
+export const DEFAULT_TX_CNT                 = 64;
 
 export const INVENTORY_SLOT_SIZE            = 36;
 export const HAND_ANIMATION_SPEED           = 20;
