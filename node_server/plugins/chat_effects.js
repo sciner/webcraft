c1d0ca079213f04bc41282f5eb96aab20e8c2398
kr0n4ik <kr0n4ik@hotmail.com>
2023-01-08 15:10:11 +0300

    add lang

--- node_server/plugins/chat_effects.js
+++ node_server/plugins/chat_effects.js
@@ -1,5 +1,3 @@
-import { ServerClient } from "../../www/js/server_client.js";
-import { Vector } from "../../www/js/helpers.js";
 import { Effect } from "../../www/js/block_type/effect.js";
 
 export default class EffectsPlugin {
@@ -9,19 +7,6 @@ export default class EffectsPlugin {
     onGame(game) {}
 
     onWorld(world) {}
-    
-    sendMessage(text, player) {
-        let packets = [
-            {
-                name: ServerClient.CMD_CHAT_SEND_MESSAGE,
-                data: {
-                    username: '<MadCraft>',
-                    text: text
-                }
-            }
-        ];
-        player.sendPackets(packets, [player], []);
-    }
 
     onChat(chat) {
         chat.onCmd(async (player, cmd, args) => {
@@ -34,27 +19,27 @@ export default class EffectsPlugin {
                 const effect = Effect.get()[id];
                 if (action == 'give') {
                     if (!effect) {
-                        this.sendMessage("Эффект с id: " + id + " не найден. Пример /effect give [id] [level] [time]", player);
+                        chat.sendSystemChatMessageToSelectedPlayers(`effect_not_found|${effect.title}`, [player.session.user_id]);
                         return false;
                     }
                     if (level < 1) {
-                        this.sendMessage("уровень эффекта < 0. Пример /effect give " + id + " [level] [time]", player);
+                        chat.sendSystemChatMessageToSelectedPlayers(`effect_error_level`, [player.session.user_id]);
                         return false;  
                     }
                     if (time < 1) {
-                        this.sendMessage("время эффекта < 0. Пример /effect give " + id + " " + level + " [time]", player);
+                        chat.sendSystemChatMessageToSelectedPlayers(`effect_error_time`, [player.session.user_id]);
                         return false;  
                     }
                     player.effects.addEffects([{id: id, level: level, time: time}]);
-                    this.sendMessage("Эффект " + effect.title + " добавлен на " + time + " сек.", player);
+                    chat.sendSystemChatMessageToSelectedPlayers(`effect_add_ok|${effect.title}|${time}`, [player.session.user_id]);
                 } else if (action == 'clear') {
                     if (!effect && id != -1) {
-                        this.sendMessage("Эффект с id: " + id + " не найден. Пример /effect give [id] [level] [time]", player);
+                        chat.sendSystemChatMessageToSelectedPlayers(`effect_not_found|${effect.title}`,[player.session.user_id]);
                         return false;
                     }
                     player.effects.delEffects(id);
                     if (id == -1) {
-                        this.sendMessage("Все эффекты удалены", player);
+                        chat.sendSystemChatMessageToSelectedPlayers(`effect_all_removed`, [player.session.user_id]);
                     } else {
                         chat.sendSystemChatMessageToSelectedPlayers(`effect_removed|${effect.title}`, [player.session.user_id]);   
                     }
