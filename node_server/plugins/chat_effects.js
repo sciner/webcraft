1fff12b8ab8c3537503cd727e2007e8b0893c09c
kr0n4ik <kr0n4ik@hotmail.com>
2023-01-08 14:36:56 +0300

    fix commands

--- node_server/plugins/chat_effects.js
+++ node_server/plugins/chat_effects.js
@@ -1,5 +1,6 @@
-import {ServerClient} from "../../www/js/server_client.js";
-import {Vector} from "../../www/js/helpers.js";
+import { ServerClient } from "../../www/js/server_client.js";
+import { Vector } from "../../www/js/helpers.js";
+import { Effect } from "../../www/js/block_type/effect.js";
 
 export default class EffectsPlugin {
     
@@ -27,15 +28,36 @@ export default class EffectsPlugin {
             if (cmd == '/effect') {
                 args = chat.parseCMD(args, ['string', 'string', 'int', 'int', 'int']);
                 const action = args[1];
-                const id = args[2];
-                const level = args[3];
-                const time = args[4];
+                const id = args[2] || -1;
+                const level = args[3] || 0;
+                const time = args[4] || 0;
+                const effect = Effect.get()[id];
                 if (action == 'give') {
+                    if (!effect) {
+                        this.sendMessage("Эффект с id: " + id + " не найден. Пример /effect give [id] [level] [time]", player);
+                        return false;
+                    }
+                    if (level < 1) {
+                        this.sendMessage("уровень эффекта < 0. Пример /effect give " + id + " [level] [time]", player);
+                        return false;  
+                    }
+                    if (time < 1) {
+                        this.sendMessage("время эффекта < 0. Пример /effect give " + id + " " + level + " [time]", player);
+                        return false;  
+                    }
                     player.effects.addEffects([{id: id, level: level, time: time}]);
+                    this.sendMessage("Эффект " + effect.title + " добавлен на " + time + " сек.", player);
                 } else if (action == 'clear') {
+                    if (!effect && id != -1) {
+                        this.sendMessage("Эффект с id: " + id + " не найден. Пример /effect give [id] [level] [time]", player);
+                        return false;
+                    }
                     player.effects.delEffects(id);
-                } else {
-                    this.sendMessage("/effect give [id] [leve] [time]", player);
+                    if (id == -1) {
+                        this.sendMessage("Все эффекты удалены", player);
+                    } else {
+                        this.sendMessage("Эффект " + effect.title + " удален", player);   
+                    }
                 }
                 return true;
             }
