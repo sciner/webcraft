4788950facc30d1fc8c59473cfda72c5862168d6
kr0n4ik <kr0n4ik@hotmail.com>
2023-01-15 15:59:49 +0300

    add options

--- node_server/server_player.js
+++ node_server/server_player.js
@@ -789,7 +789,8 @@ export class ServerPlayer extends Player {
         if (!data.destroyBlock || this.game_mode.isCreative()) {
             return true;
         }
-        const world_block = this.world.getBlock(new Vector(data.pos));
+        const world = this.world;
+        const world_block = world.getBlock(new Vector(data.pos));
         if (!world_block) {
             return false;
         }
@@ -797,12 +798,12 @@ export class ServerPlayer extends Player {
         if (!block) {
             return false;
         }
-        const head = this.world.getBlock(this.getEyePos());
+        const head = world.getBlock(this.getEyePos());
         if (!head) {
             return false;
         }
         const instrument = BLOCK.fromId(this.state.hands.right.id);
-        let mul = 1;
+        let mul = world.getGeneratorOptions('tool_mining_speed', 1);
         mul *= (head.id == 0 && (head.fluid & FLUID_TYPE_MASK) === FLUID_WATER_ID) ? 0.2 : 1;
         mul += mul * 0.2 * this.effects.getEffectLevel(Effect.HASTE); // Ускоренная разбивка блоков
         mul -= mul * 0.2 * this.effects.getEffectLevel(Effect.MINING_FATIGUE); // усталость
