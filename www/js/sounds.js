d695751a8573cf1f35d8f8459bb21ed5e2267131
SCINER <sciner@ya.ru>
2023-02-03 01:36:51 +0400

    change music volume

--- www/js/sounds.js
+++ www/js/sounds.js
@@ -45,7 +45,6 @@ class Music {
      * @param {Number} volume from 0 to 1.
      */
     set volume(volume) {
-        volume /= 5
         this.#volume = volume
         this.howl?.volume(this._getTrackVolume())
     }
