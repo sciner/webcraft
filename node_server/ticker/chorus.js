767d45cdfa66139a14fc5c94eb2274c78fcd18b6
kr0n4ik <kr0n4ik@hotmail.com>
2023-02-07 07:48:14 +0300

    ticker

--- node_server/ticker/chorus.js
+++ node_server/ticker/chorus.js
@@ -10,7 +10,21 @@ export default class Ticker {
     
     //
     static func(tick_number, world, chunk, v) {
-        console.log('test')
+        if (Math.random() > 0.1) {
+            return
+        }
+        const tblock = v.tblock
+        const extra_data = tblock.extra_data
+        const pos = v.pos.clone()
+        const up = world.getBlock(pos.offset(0, 1, 0))
+        if (up && up.id == BLOCK.AIR.id) {
+            const age = extra_data.stage;
+            if (age < 5) {
+                
+
+            }
+            console.log(extra_data.stage)
+        }
     }
 
 }
