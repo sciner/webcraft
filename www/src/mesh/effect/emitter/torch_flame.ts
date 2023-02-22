46a60f73e98ffc9e732f95aa3801c7d5a6b4748b
Lenar <sciner@users.noreply.github.com>
2023-02-22 16:17:53 +0300

    Update torch_flame.ts

--- www/src/mesh/effect/emitter/torch_flame.ts
+++ www/src/mesh/effect/emitter/torch_flame.ts
@@ -1,4 +1,4 @@
-import { IndexedColor, QUAD_FLAGS, Vector } from "../../../helpers.js";
+import { getChunkAddr, IndexedColor, Vector } from "../../../helpers.js";
 import { DEFAULT_EFFECT_MATERIAL_KEY, getEffectTexture } from "../../effect.js";
 import { Mesh_Effect_Particle } from "../particle.js";
 
@@ -18,7 +18,6 @@ export default class emitter {
         const m             = this.material_key.split('/');
         const resource_pack = Qubatch.world.block_manager.resource_pack_manager.get(m[0]);
         this.material       = resource_pack.getMaterial(this.material_key);
-        this.ticks          = 0;
     }
 
     /**
@@ -39,35 +38,22 @@ export default class emitter {
             return [];
         }
 
-        if(this.ticks++ > 0) {
-            return [];
-        }
-
         const {texture, texture_index} = getEffectTexture(emitter.textures);
 
         // новая частица
         const particle = new Mesh_Effect_Particle({
-            // old
-            // life:           1 + Math.random(),
-            // velocity:       new Vector(0, texture_index > 1 ? .5 : 0, 0),
-            // flags:          QUAD_FLAGS.NO_CAN_TAKE_LIGHT,
-            // new
-            life:           1000000,
-            velocity:       new Vector(0, 0, 0),
-            flags:          0,
-            force_flags:    QUAD_FLAGS.NO_CAN_TAKE_LIGHT | QUAD_FLAGS.FLAG_TORCH_FLAME | QUAD_FLAGS.LOOK_AT_CAMERA_HOR | QUAD_FLAGS.NORMAL_UP,
-            //
-            has_physics:    false,
+            life:           1 + Math.random(),
             texture:        texture,
+            has_physics:    false,
             size:           1/8,
             scale:          1,
-            smart_scale:    {0: 1, 1: 1},
+            velocity:       new Vector(0, texture_index > 1 ? .5 : 0, 0),
             ag:             new Vector(0, 0, 0),
             pp:             this.pp,
             material_key:   this.material_key,
             pos:            this.pos.clone().addScalarSelf(
                 (Math.random() - Math.random()) * 0.01,
-                .2 + .25,
+                .2,
                 (Math.random() - Math.random()) * 0.01
             ),
             material:       this.material
