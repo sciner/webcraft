f2848a1a3a0d649133c8894232fe22f817382c23
kr0n4ik <kr0n4ik@hotmail.com>
2023-03-08 23:33:44 +0300

    add shift

--- www/src/terrain_generator/biome3/layers/end.ts
+++ www/src/terrain_generator/biome3/layers/end.ts
@@ -25,13 +25,11 @@ export default class Biome3LayerEnd {
     }
 
     generate(chunk : ChunkWorkerChunk, seed, rnd) {
-
         // Cluster
         chunk.timers.start('generate_cluster')
         chunk.cluster = this.clusterManager.getForCoord(chunk.coord, null) ?? null
         chunk.cluster.fillBlocks(null, chunk, null, false, false)
         chunk.timers.stop()
-
         const BLOCK = this.generator.block_manager
         const { cx, cy, cz, cw, uint16View } = chunk.tblocks.dataChunk
         const block_id = BLOCK.END_STONE.id
@@ -40,11 +38,8 @@ export default class Biome3LayerEnd {
                 for (let y = 0; y < chunk.size.y; y++) {
                     const n2 = -this.noise2d((chunk.coord.x + x) / 100, (chunk.coord.z + z) / 100) * y
                     const n1 = this.noise2d((chunk.coord.x + x) / 100, (chunk.coord.z + z) / 100) * 36
-                    const tx = (chunk.coord.x + x - 100) % 1024
-                    const tz = (chunk.coord.z + z - 110) % 1024
-                    const n3 = this.noise2d((tx + 8) / 100, (tz + 30) / 100) * y
                     const index = cx * x + cy * y + cz * z + cw
-                    if (((n2 > 5 && y < 31) || (-n1 > (y - 26) && y > 30) || (n3 > 12 && tx < 80 && tz < 80)) && chunk.addr.y == 0) {
+                    if (((n2 > 5 && y < 31) || (-n1 > (y - 26) && y > 30)) && chunk.addr.y == 0) {
                         uint16View[index] = block_id
                     }
                 }
