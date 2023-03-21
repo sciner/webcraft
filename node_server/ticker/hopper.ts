b8d98bda8914c7a9e0ef1a4c781b5b25c51d303e
kr0n4ik <kr0n4ik@hotmail.com>
2023-03-21 15:55:31 +0300

    add side

--- node_server/ticker/hopper.ts
+++ node_server/ticker/hopper.ts
@@ -21,11 +21,11 @@ export default class Ticker {
         const pos = v.pos.clone()
         const neighbours = tblock.getNeighbours(world, BLOCK_CACHE)
         const updated = []
-        const getItem = () => {
+        const getStackItem = () => {
             for (let i = 0; i < 5; i++) {
                 if (tblock.extra_data.slots[i]) {
                     const item = tblock.extra_data.slots[i]
-                    delete (tblock.extra_data.slots[i])
+                    delete(tblock.extra_data.slots[i])
                     updated.push({ pos: v.pos.clone(), item: tblock.convertToDBItem(), action_id: ServerClient.BLOCK_ACTION_MODIFY })
                     world.chests.sendChestToPlayers(tblock, null)
                     return item
@@ -33,44 +33,48 @@ export default class Ticker {
             }
             return null
         }
-        const decItem = (i) => {
+        const decItem = (id) => {
             for (let i = 0; i < 5; i++) {
-                if (tblock.extra_data.slots[i]) {
-                
+                if (tblock.extra_data.slots[i]?.id == id) {
+                    tblock.extra_data.slots[i].count--
+                    if (tblock.extra_data.slots[i].count <= 0) {
+                        delete (tblock.extra_data.slots[i])
+                    }
+                    updated.push({ pos: v.pos.clone(), item: tblock.convertToDBItem(), action_id: ServerClient.BLOCK_ACTION_MODIFY })
+                    world.chests.sendChestToPlayers(tblock, null)
+                    return true
                 }
             }
+            return false
         }
 
-
-        /*let item = null
-
-        for (let i = 0; i < 5; i++) {
-            if (extra_data.slots[i]) {
-                item = extra_data.slots[i]
-                break
+        const getSide = () => {
+            for (const side of ['WEST', 'EAST', 'NORTH', 'SOUTH']) {
+                if ([bm.FURNACE.id].includes(neighbours[side]?.id)) {
+                    return neighbours[side]
+                }
             }
-        }
+            return null
+        } 
 
-        // если есть предмет
-        if (item) {
-            if (neighbours.DOWN.id == bm.FURNACE.id) {
-                
-            }
-        }
-
-*/
-        //console.log(neighbours.DOWN.id)
-        if (neighbours.DOWN.id == bm.FURNACE.id) {
+        // если под воронкой блок который производит
+        if ([bm.FURNACE.id].includes(neighbours.DOWN.id)) {
             if (!neighbours.DOWN.extra_data.slots[0]) {
-                const item = getItem()
-                if (item) {
-                    neighbours.DOWN.extra_data.slots[0] = item
-                    console.log(neighbours.DOWN.pos)
-                    console.log(neighbours.DOWN.convertToDBItem())
+                const items = getStackItem()
+                if (items) {
+                    neighbours.DOWN.extra_data.slots[0] = items
+                    updated.push({pos: neighbours.DOWN.posworld, item: neighbours.DOWN.convertToDBItem(), action_id: ServerClient.BLOCK_ACTION_MODIFY})
+                }
+            } else {
+                const item = neighbours.DOWN.extra_data.slots[0]
+                if (item.count < 64 && decItem(item.id)) {
+                    neighbours.DOWN.extra_data.slots[0].count++
                     updated.push({pos: neighbours.DOWN.posworld, item: neighbours.DOWN.convertToDBItem(), action_id: ServerClient.BLOCK_ACTION_MODIFY})
                 }
             }
         }
+        
+
 
         return updated
     }
