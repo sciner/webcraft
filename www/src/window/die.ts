6aeee55315bb9389e3efe56650c53c362e3e49f5
sciner <sciner2008@gmail.com>
2023-03-23 20:42:34 +0400

    fix die and barrel window backgrounds

--- www/src/window/die.ts
+++ www/src/window/die.ts
@@ -4,7 +4,6 @@ import { Lang } from "../lang.js";
 import { BlankWindow } from "./blank.js";
 
 export class DieWindow extends BlankWindow {
-    [key: string]: any;
 
     constructor() {
         super(10, 10, 352, 332, 'frmDie')
@@ -29,62 +28,20 @@ export class DieWindow extends BlankWindow {
         super.onShow(args)
     }
 
+    // Exit button
     addCloseButton() {
-        // Close button
         const btnClose = this.btnClose = new Button(20 * this.zoom, 150 * this.zoom, this.w - 40 * this.zoom, 40 * this.zoom, 'btnClose', Lang.btn_exit)
-        btnClose.style.background.color = '#888888ff'
-        btnClose.style.font.color = '#ffffffff'
-        /*
-        btnClose.style.font.shadow = {
-            enable: true,
-            x: 2 * this.zoom,
-            y: 2 * this.zoom,
-            blur: 0,
-            color: 'rgba(0, 0, 0, 0.5)'
-        }
-        */
         btnClose.onMouseDown = function (e) {
             Qubatch.exit();
-        }
-        btnClose.onMouseEnter = function () {
-            this.style.background.color = '#8892c9';
-            this.style.background.image_save = this.style.background.image;
-            this.style.background.image = null;
-        }
-        btnClose.onMouseLeave = function () {
-            this.style.background.color = '#777777ff';
-            this.style.background.image = this.style.background.image_save;
-            this.style.background.image_save = null;
         }
         this.add(btnClose);
     }
 
     addReturnButton() {
         const btnReturn = this.btnReturn = new Button(20 * this.zoom, 100 * this.zoom, this.w - 40 * this.zoom, 40 * this.zoom, 'btnReturn', Lang.btn_return)
-        btnReturn.style.background.color = '#888888ff'
-        btnReturn.style.font.color = '#ffffffff'
-        /*
-        btnReturn.style.font.shadow = {
-            enable: true,
-            x: 2 * this.zoom,
-            y: 2 * this.zoom,
-            blur: 0,
-            color: 'rgba(0, 0, 0, 0.5)'
-        }
-        */
         btnReturn.onMouseDown = (e) => {
             this.getRoot().closeAll();
             Qubatch.player.world.server.Send({ name: ServerClient.CMD_RESURRECTION });
-        }
-        btnReturn.onMouseEnter = function () {
-            this.style.background.color = '#8892c9';
-            this.style.background.image_save = this.style.background.image;
-            this.style.background.image = null;
-        }
-        btnReturn.onMouseLeave = function () {
-            this.style.background.color = '#777777ff';
-            this.style.background.image = this.style.background.image_save;
-            this.style.background.image_save = null;
         }
         this.add(btnReturn);
     }
