c89e8f041602a929985e453c4b42153cf1743a26
kr0n4ik <kr0n4ik@hotmail.com>
2023-03-29 19:25:38 +0300

    Fix mopbs onPanic pitch (#551)

--- www/src/control/player_control.ts
+++ www/src/control/player_control.ts
@@ -43,6 +43,7 @@ export interface IPlayerControls {
     jump ?      : boolean
     sprint ?    : boolean
     sneak ?     : boolean
+    pitch ?     : boolean
 }
 
 /** It stores and processes player's input on the client. */
@@ -131,7 +132,7 @@ export abstract class PlayerControl {
         controls.forward = params.forward
         controls.jump = params.jump
         controls.sneak = params.sneak
-
+        controls.pitch = params.pitch
         // TODO do something with MobControlParams.pitch, or don't accept it
     }
 
