5f1b3722b7855d357baf3a46bc70d4d7abe1fc76
Lenar <sciner@users.noreply.github.com>
2023-04-04 20:35:42 +0300

    Perf culling scanline (#567)
    * wip
    * wip scanline
    * backtrack a bit
    * cull cycle ready
    * move cullID to separate array
    * start debug
    * IT works! and it logs a bit
    * process frustum corners
    * why did it even work before?
    * optimize ChunkMesh.draw()

--- www/src/helpers/spiral_generator.ts
+++ www/src/helpers/spiral_generator.ts
@@ -1,23 +1,18 @@
 import {Vector} from "./vector.js";
 
 export class SpiralEntry {
-    [key: string]: any;
-    dist: number;
-    pos: Vector;
-    chunk: any;
-    newValue: any;
+    pos = new Vector();
+    chunk: any = null;
+    newValue: any = null;
     translated = false;
-    constructor() {
-        this.pos = new Vector();
-        this.dist = 0;
-        this.chunk = null;
-        this.newValue = null;
-    }
+    dist = 0;
+    indexYZ = 0;
 
     copyTranslate(se, translation) {
         this.pos.copyFrom(se.pos);
         this.pos.addSelf(translation);
         this.dist = se.dist;
+        this.indexYZ = se.indexYZ;
         this.chunk = null;
         this.translated = false;
         return this;
@@ -35,32 +30,34 @@ export class SpiralGenerator {
 
     // generate ...
     static generate(margin: int) {
-        let size : number = margin * 2;
-        if(SpiralGenerator.cache.has(margin)) {
+        let size: number = margin * 2;
+        if (SpiralGenerator.cache.has(margin)) {
             return SpiralGenerator.cache.get[margin];
         }
         var resp = [];
-        function rPush(vec : IVector) {
+
+        function rPush(vec: IVector) {
             // Если позиция на расстояние видимости (считаем честно, по кругу)
             let x = vec.x - size / 2;
             let z = vec.z - size / 2;
             let dist = Math.sqrt(x * x + z * z);
-            if(dist < margin) {
+            if (dist < margin) {
                 resp.push(vec);
             }
         }
+
         let iInd = Math.trunc(size / 2);
         let jInd = Math.trunc(size / 2);
         let iStep = 1;
         let jStep = 1;
         rPush(new Vector(iInd, 0, jInd));
-        for(let i = 0; i < size; i++) {
+        for (let i = 0; i < size; i++) {
             for (let h = 0; h < i; h++) rPush(new Vector(iInd, 0, jInd += jStep));
             for (let v = 0; v < i; v++) rPush(new Vector(iInd += iStep, 0, jInd));
             jStep = -jStep;
             iStep = -iStep;
         }
-        for(let h = 0; h < size - 1; h++) {
+        for (let h = 0; h < size - 1; h++) {
             rPush(new Vector(iInd, 0, jInd += jStep));
         }
         SpiralGenerator.cache.set(margin, resp);
@@ -72,42 +69,44 @@ export class SpiralGenerator {
      * @param {Vector} vec_margin
      * @returns
      */
-    static generate3D(vec_margin : IVector) : SpiralGrid {
+    static generate3D(vec_margin: IVector): SpiralGrid {
         const cache_key = vec_margin.toString();
-        if(SpiralGenerator.cache3D.hasOwnProperty(cache_key)) {
+        if (SpiralGenerator.cache3D.hasOwnProperty(cache_key)) {
             return SpiralGenerator.cache3D[cache_key];
         }
         const resp = new SpiralGrid(vec_margin);
         const center = new Vector(0, 0, 0);
-        const exists : string[] = [];
-        const MAX_DIST  = vec_margin.x;
-        for(let y = -vec_margin.y; y <= vec_margin.y; y++) {
-            for(let x = -vec_margin.x; x <= vec_margin.x; x++) {
-                for(let z = -vec_margin.z; z <= vec_margin.z; z++) {
-                    let vec = new Vector(x, y, z);
-                    let dist = Math.round(vec.distance(center) * 1000) / 1000;
-                    if(dist <= MAX_DIST) {
-                        let key = vec.toString();
-                        if(exists.indexOf(key) < 0) {
-                            const entry = new SpiralEntry();
-                            entry.pos = vec;
-                            entry.dist = dist;
-                            resp.entries.push(entry);
-                            exists.push(key)
-                        }
-                    }
+        const MAX_DIST = vec_margin.x;
+        const indexByYZ = resp.size.indexByYZ = [];
+        const radByYZ = resp.size.radByYZ = [];
+        const startByYZ = resp.size.startByYZ = [];
+        let cnt = 0;
+        for (let y = -vec_margin.y; y <= vec_margin.y; y++) {
+            for (let z = -vec_margin.z; z <= vec_margin.z; z++) {
+                const d = Math.min(vec_margin.x, Math.floor(Math.sqrt(MAX_DIST * MAX_DIST - y * y - z * z)));
+                radByYZ.push(d >= 0 ? d : -1);
+                startByYZ.push(cnt);
+                if (d < 0) {
+                    continue;
+                }
+                for (let x = -d; x <= d; x++) {
+                    const entry = new SpiralEntry();
+                    entry.indexYZ = cnt++;
+                    entry.pos.set(x, y, z);
+                    entry.dist = Math.round(entry.pos.distance(center) * 1000) / 1000;
+                    resp.entries.push(entry);
                 }
             }
         }
-        resp.entries.sort(function(a : SpiralEntry, b : SpiralEntry) {
+        startByYZ.push(cnt);
+        resp.entriesByYZ = resp.entries.slice(0);
+        resp.size.len = radByYZ.length;
+        resp.entries.sort(function (a: SpiralEntry, b: SpiralEntry) {
             return a.dist - b.dist;
         });
-        resp.sortedNumByIndex = new Array(resp.entries.length);
         for (let i = 0; i < resp.entries.length; i++) {
-            const entry = resp.entries[i];
-            resp.sortedNumByIndex[resp.size.arrayIndByVec(entry.pos)] = i;
+            indexByYZ[resp.entries[i].indexYZ] = i;
         }
-
         SpiralGenerator.cache3D[cache_key] = resp;
         return resp;
     }
@@ -118,41 +117,39 @@ export class SpiralGenerator {
  */
 export class SpiralSize {
     marginVec = new Vector();
+    radByYZ: number[] = null;
+    indexByYZ: number[] = null;
+    startByYZ: number[] = null;
 
-    cx: number;
-    cy: number;
-    cz: number;
-    cw: number;
+    depth: number;
+    dw: number;
+    len: number;
+
     /**
      * number of coord indices for spiral of this size
      */
-    outerLen: number;
-
     constructor(marginVec: IVector) {
         this.marginVec.copyFrom(marginVec);
 
-        this.cx = this.marginVec.y * 2 + 1;
-        this.cy = 1;
-        this.cz = this.cx * (this.marginVec.x * 2 + 1);
-        this.cw = (this.cx * this.marginVec.x + this.cy * this.marginVec.y + this.cz * this.marginVec.z);
-        this.outerLen = this.cz * (this.marginVec.z * 2 + 1);
+        this.depth = this.marginVec.z * 2 + 1;
+        this.dw = this.marginVec.y * this.depth + this.marginVec.z;
+        this.len = 0;
     }
 
     equal(other: SpiralSize) {
         return this.marginVec.equal(other.marginVec);
     }
 
-    containsXYZ(dx: number, dy: number, dz: number) {
-        const {marginVec} = this;
-        return Math.abs(dx) <= marginVec.x && Math.abs(dy) <= marginVec.y && Math.abs(dz) <= marginVec.z;
-    }
-
     arrayIndByVec(dvec: IVector) {
-        const {marginVec} = this;
-        if (Math.abs(dvec.x) <= marginVec.x && Math.abs(dvec.y) <= marginVec.y && Math.abs(dvec.z) <= marginVec.z) {
-            return this.cx * dvec.x + this.cy * dvec.y + this.cz * dvec.z + this.cw;
+        if (Math.abs(dvec.y) > this.marginVec.y || Math.abs(dvec.z) > this.marginVec.z) {
+            return -1;
         }
-        return -1;
+        let yz = dvec.y * this.depth + dvec.z + this.dw;
+        let rad = this.radByYZ[yz];
+        if (Math.abs(dvec.x) > rad) {
+            return -1;
+        }
+        return this.startByYZ[yz] + dvec.x + rad;
     }
 }
 
@@ -161,36 +158,37 @@ const deltaVec = new Vector();
 
 export class SpiralGrid {
     entries: Array<SpiralEntry> = [];
+    entriesByYZ: SpiralEntry[] = [];
     center = new Vector();
     size: SpiralSize = null;
-    sortedNumByIndex: number[] = null;
+    cullIDs: number[] = [];
 
     constructor(marginVec: IVector = new Vector()) {
         this.size = new SpiralSize(marginVec);
     }
 
     make(center: Vector, marginVec: IVector) {
-        const {entries} = this;
+        const {entries, entriesByYZ, cullIDs} = this;
 
         const template = SpiralGenerator.generate3D(marginVec);
         const spiral_moves_3d = template.entries;
 
-        this.size = new SpiralSize(marginVec);
+        const size = this.size = template.size;
         this.center.copyFrom(center);
-
-        this.sortedNumByIndex = template.sortedNumByIndex;
 
         while (entries.length < spiral_moves_3d.length) {
             entries.push(new SpiralEntry());
+            cullIDs.push(-1);
         }
-        let n = entries.length = spiral_moves_3d.length;
+        let n = entries.length = entriesByYZ.length = cullIDs.length = spiral_moves_3d.length;
         for (let i = 0; i < n; i++) {
             entries[i].copyTranslate(spiral_moves_3d[i], center);
+            entriesByYZ[i] = entries[size.indexByYZ[i]];
         }
     }
 
     translate(newCenter: IVector) {
-        const {entries, size, sortedNumByIndex} = this;
+        const {entries, entriesByYZ, size} = this;
         deltaVec.copyFrom(newCenter).subSelf(this.center);
         for (let i = 0; i < entries.length; i++) {
             const entry = entries[i];
@@ -201,14 +199,8 @@ export class SpiralGrid {
             if (ind < 0) {
                 continue;
             }
-            ind = sortedNumByIndex[ind];
-            if (ind < 0) {
-                continue;
-            }
-            if (entries[ind]) {
-                entry.translated = true;
-                entry.newValue = entries[ind].chunk;
-            }
+            entry.translated = true;
+            entry.newValue = entriesByYZ[ind].chunk;
         }
         //TODO: events on add/remove here
         for (let i = 0; i < entries.length; i++) {
@@ -232,13 +224,7 @@ export class SpiralGrid {
         if (ind < 0) {
             return;
         }
-        ind = this.sortedNumByIndex[ind];
-        if (ind < 0) {
-            return;
-        }
         //TODO: events on add/remove here
-        if (this.entries[ind]) {
-            this.entries[ind].chunk = chunk;
-        }
+        this.entriesByYZ[ind].chunk = chunk;
     }
 }
