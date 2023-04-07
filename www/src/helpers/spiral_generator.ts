27cee2911ed771daeeeb1f69b2de956cc35a4c78
sciner <sciner2008@gmail.com>
2023-04-08 03:25:33 +0400

    remove || !this.sortedNumByIndex

--- www/src/helpers/spiral_generator.ts
+++ www/src/helpers/spiral_generator.ts
@@ -221,7 +221,7 @@ export class SpiralGrid {
     setChunk(absAddr: Vector, chunk: any) {
         tempVec.copyFrom(absAddr).subSelf(this.center);
         let ind = this.size.arrayIndByVec(tempVec);
-        if (ind < 0 || !this.sortedNumByIndex) {
+        if (ind < 0) {
             return;
         }
         //TODO: events on add/remove here
