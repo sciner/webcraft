a1f4294403d7c6c8a33bfb5bc327d0420a5a181e
kr0n4ik <kr0n4ik@hotmail.com>
2023-04-08 11:06:43 +0300

    complete

--- www/src/window/quest/view.ts
+++ www/src/window/quest/view.ts
@@ -18,7 +18,7 @@ export class QuestView extends Window {
     [key: string]: any;
 
     //
-    constructor(x, y, w, h, id, title?, text?) {
+    constructor(x, y, w, h, id, parent: Window, title?, text?) {
 
         super(x, y, w, h, id, title, text);
 
@@ -27,6 +27,7 @@ export class QuestView extends Window {
         this.wheel_scroll = 36 * this.zoom;
         this.style.background.color = '#ffffff22';
         this.style.border.hidden = true;
+        this.parent = parent
 
         this.clip()
 
@@ -42,16 +43,22 @@ export class QuestView extends Window {
         this.scrollY += Math.sign(e.original_event.wheelDeltaY) * this.wheel_scroll
         this.scrollY = Helpers.clamp(this.scrollY, min_scroll, 0)
         this.container.y = this.scrollY
+        this.parent.scrollbar.value = -this.scrollY
+    }
+
+    updateScroll(val) {
+        const min_scroll = Math.min(this.h - this.container.h, 0)
+        this.scrollY = val * this.wheel_scroll
+        this.scrollY = Helpers.clamp(this.scrollY, min_scroll, 0)
+        this.container.y = this.scrollY
     }
 
     show(quest) {
-
         const ql = this.getWindow('questViewLayout');
         const lblTitle = ql.getWindow('lblTitle');
         const lDesc = ql.getWindow('lDesc');
         const lblActions = ql.getWindow('lblActions');
         const lblRewards = ql.getWindow('lblRewards');
-        const slider = ql.getWindow('slider')
                 //
         lblTitle.text = quest.title
         lDesc.text = quest.description
@@ -111,6 +118,8 @@ export class QuestView extends Window {
 
         ql.refresh()
 
+        this.updateScroll(0)
+        this.parent.scrollbar.max = this.container.h -this.h
     }
 
 }
