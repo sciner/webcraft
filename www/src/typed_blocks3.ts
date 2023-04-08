2ca1c2adc46c1845c33cd5993b489454de75833c
ExcuseMe <ExcuseMe@users.noreply.github.com>
2023-04-08 14:08:03 +0300

    asphyxiation uses fluid level

--- www/src/typed_blocks3.ts
+++ www/src/typed_blocks3.ts
@@ -977,6 +977,15 @@ export class TBlock {
         return this.fluid === 0 || material === BLOCK.BUBBLE_COLUMN
     }
 
+    hasOxygenAt(worldPos: IVector): boolean {
+        const material = this.material
+        if (!material.has_oxygen) {
+            return false
+        }
+        return worldPos.y > this.getFluidLevel(worldPos.x, worldPos.z) ||
+            material === BLOCK.BUBBLE_COLUMN
+    }
+
     //
     get pos() : Vector {
         return this.vec;
