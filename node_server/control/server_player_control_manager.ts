ab4325d6c794cf3478a9018f99ffd7dfa06544e5
ExcuseMe <ExcuseMe@users.noreply.github.com>
2023-04-10 19:04:15 +0300

    Fix ServerPlayerConrolManager constructor (#588)
    * fix ServerConrolManager constructor
    * down @swc version, because new version has perf problem

--- node_server/control/server_player_control_manager.ts
+++ node_server/control/server_player_control_manager.ts
@@ -48,6 +48,11 @@ export class ServerPlayerControlManager extends PlayerControlManager {
 
     constructor(player: ServerPlayer) {
         super(player as any as Player)
+        // super constructor doesn't call these methods correctly, so call them here
+        this.physicsSessionId = -1 // revert to what it was before the super constructor
+        const pos = new Vector(player.sharedProps.pos)
+        this.updateCurrentControlType(false)
+        this.startNewPhysicsSession(pos)
     }
 
     get serverPlayer(): ServerPlayer { return this.player as any as ServerPlayer }
