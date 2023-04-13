e26b4cc7d1aa8c6fc07d9da4a54c40afa331c75e
sciner <sciner2008@gmail.com>
2023-04-13 21:28:33 +0400

    f

--- www/src/ui/bbmodel_preview.ts
+++ www/src/ui/bbmodel_preview.ts
@@ -67,12 +67,12 @@ export class BBModel_Preview {
         this.render.player = player
 
         // Init blocks
-        // const blockTask = BLOCK.init(world.settings)
-        // await Promise.all([blockTask])
-        BLOCK.reset()
-        BLOCK.resource_pack_manager = new ResourcePackManager(BLOCK)
-        await BLOCK.resource_pack_manager.init(world.settings)
-        BLOCK.reset()
+        const blockTask = BLOCK.init(world.settings)
+        await Promise.all([blockTask])
+        // BLOCK.reset()
+        // BLOCK.resource_pack_manager = new ResourcePackManager(BLOCK)
+        // await BLOCK.resource_pack_manager.init(world.settings)
+        // BLOCK.reset()
 
         world.players = new PlayerManager(world)
         world.mobs = new MobManager(world)
