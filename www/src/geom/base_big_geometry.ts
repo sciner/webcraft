0d9e53301ba7fd0976b71352fa6d4adbcb87aa70
sciner <sciner2008@gmail.com>
2023-04-20 14:03:45 +0400

    fix

--- www/src/geom/base_big_geometry.ts
+++ www/src/geom/base_big_geometry.ts
@@ -12,6 +12,7 @@ export interface BigGeometryOptions {
 }
 
 export class BaseBigGeometry {
+    staticCopy: any;
     staticSize: number;
     dynamicSize: number;
     useTransformFeedback: boolean;
