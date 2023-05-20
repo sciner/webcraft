e65122181a1b85d17313a2b1b83422356d38202e
kr0n4ik <kr0n4ik@hotmail.com>
2023-05-20 16:54:58 +0300

    del rotate

--- www/src/world_action.ts
+++ www/src/world_action.ts
@@ -1238,7 +1238,7 @@ export async function doBlockAction(e, world, action_player_info: ActionPlayerIn
                 }
             }
             // 
-            new_item.rotate.z = pos.point.y < .5 ? -1 : 1
+            
             // Rotate block one of 8 poses
             if(mat_block.tags.includes('rotate_x8')) {
                 if(new_item.rotate.y != 0) {
