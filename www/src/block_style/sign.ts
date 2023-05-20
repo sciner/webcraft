c705bf3c5f0b5b1eb568f923c04acefe6362d109
kr0n4ik <kr0n4ik@hotmail.com>
2023-05-20 14:01:48 +0300

    add half

--- www/src/block_style/sign.ts
+++ www/src/block_style/sign.ts
@@ -49,11 +49,38 @@ export default class style {
         let x           = 0;
         let y           = 0;
         let z           = 0;
-        let aabb        = null;
+        let aabb        = null
+        let aabb2       = null
         const resp      = [];
         const width     = .5;
         const height    = 1;
 
+        if (tblock.rotate.y == 0) {
+            if (tblock.rotate.z == -1) {
+                aabb = new AABB()
+                aabb.set(
+                    1/16,  4/16,  0/16,
+                    15/16, 12/16, 2/16,
+                )
+                const dir = CubeSym.dirAdd((tblock.rotate.x + 2) % 4, CubeSym.ROT_Y2)
+                aabb.rotate(dir, new Vector(0.5, 0.5, 0.5))
+                return [aabb]
+            }
+            aabb = new AABB()
+            aabb.set(
+                1/16,  2/16,  7/16,
+                15/16, 10/16, 9/16,
+            )
+            const dir = CubeSym.dirAdd((tblock.rotate.x + 1) % 4, CubeSym.ROT_Y2)
+            aabb.rotate(dir, new Vector(0.5, 0.5, 0.5))
+            aabb2 = new AABB()
+            aabb2.set(
+                0,  14/16,  6/16,
+                1, 1, 10/16,
+            )
+            aabb2.rotate(dir, new Vector(0.5, 0.5, 0.5))
+            return [aabb, aabb2]
+        }
         /* Center
         if(tblock.rotate.y == 0) {
             const mul = 1.01;
@@ -70,7 +97,7 @@ export default class style {
             const dir = CubeSym.dirAdd(tblock.rotate.x, CubeSym.ROT_Y2);
             aabb.rotate(dir, aabb.center);
             aabb.translate(cubeSymAxis[dir][0] * dist, -(.2 + aabb.height) / 2, cubeSymAxis[dir][1] * dist);
-        } else {*/
+        }*/
             aabb = new AABB();
             aabb.set(
                 x + .5 - width/2,
@@ -80,8 +107,8 @@ export default class style {
                 y + height,
                 z + .5 + width/2,
             );
-            resp.push(aabb);
-      //  }
+    
+     
 
         return [aabb];
 
@@ -99,36 +126,7 @@ export default class style {
         const c_chain = bm.calcTexture(bm.CHAIN.texture, DIRECTION.UP)
         const rotate = block.rotate
         const parts = []
-        if (rotate.y == 1) {
-            parts.push(...[
-                {
-                    "size": {"x": 14, "y": 8, "z": 2},
-                    "translate": {"x": 0, "y": 4, "z": 0},
-                    "faces": {
-                        "up": {"uv": [8, 8],"texture": c_up},
-                        "down": {"uv": [8, 8],"texture": c_up},
-                        "north": {"uv": [8, 8],"texture": c_up},
-                        "south": {"uv": [8, 8],"texture": c_up},
-                        "east": {"uv": [8, 8],"texture": c_up},
-                        "west": {"uv": [8, 8],"texture": c_up}
-                    }
-                },
-                {
-                    "size": {"x": 2, "y": 8, "z": 2},
-                    "translate": {"x": 0, "y": -4, "z": 0},
-                    "faces": {
-                        "up": {"uv": [8, 8],"texture": c_up},
-                        "down": {"uv": [8, 8],"texture": c_up},
-                        "north": {"uv": [8, 8],"texture": c_up},
-                        "south": {"uv": [8, 8],"texture": c_up},
-                        "east": {"uv": [8, 8],"texture": c_up},
-                        "west": {"uv": [8, 8],"texture": c_up}
-                    }
-                }
-            ])
-        }
-        /*if (rotate.y == 0) {
-            if (rotate.z == -1) {
+        if (rotate.z == -1 && rotate.y == 0) {
                 parts.push(
                     {
                         "size": {"x": 14, "y": 8, "z": 2},
@@ -143,49 +141,23 @@ export default class style {
                         }
                     }
                 )
-            } else {
-                parts.push(
-                    {
-                        "size": {"x": 14, "y": 8, "z": 2},
-                        "translate": {"x": 0, "y": rotate.y == 1 ? 4 : -2, "z": 0},
-                        "faces": {
-                            "up": {"uv": [8, 8],"texture": c_up},
-                            "down": {"uv": [8, 8],"texture": c_up},
-                            "north": {"uv": [8, 8],"texture": c_up},
-                            "south": {"uv": [8, 8],"texture": c_up},
-                            "east": {"uv": [8, 8],"texture": c_up},
-                            "west": {"uv": [8, 8],"texture": c_up}
-                        }
-                    }
-                )    
-            }
-        }
-        if (rotate.x == 0 || rotate.y == -1) {
-
-        }
-        
-
-
-        parts.push(
-            {
-                "size": {"x": 2, "y": 8, "z": 14},
-                "translate": {"x": 0, "y": rotate.y == 1 ? 4 : -2, "z": 0},
-                "faces": {
-                    "up": {"uv": [8, 8],"texture": c_up},
-                    "down": {"uv": [8, 8],"texture": c_up},
-                    "north": {"uv": [8, 8],"texture": c_up},
-                    "south": {"uv": [8, 8],"texture": c_up},
-                    "east": {"uv": [8, 8],"texture": c_up},
-                    "west": {"uv": [8, 8],"texture": c_up}
-                }
-            }
-        )
-        // напольный
-        if (rotate.y == 1) {
-            parts.push(
+        } else if (rotate.y == 1) {
+            parts.push(...[
                 {
-                    "size": {"x": 2, "y": 8, "z": 2},
-                    "translate": {"x": 0, "y": -4, "z": 0},
+                    "size": {"x": 14, "y": 8, "z": 2},
+                    "translate": {"x": 0, "y": 5.5, "z": 0},
+                    "faces": {
+                        "up": {"uv": [8, 8],"texture": c_up},
+                        "down": {"uv": [8, 8],"texture": c_up},
+                        "north": {"uv": [8, 8],"texture": c_up},
+                        "south": {"uv": [8, 8],"texture": c_up},
+                        "east": {"uv": [8, 8],"texture": c_up},
+                        "west": {"uv": [8, 8],"texture": c_up}
+                    }
+                },
+                {
+                    "size": {"x": 2, "y": 9, "z": 2},
+                    "translate": {"x": 0, "y": -3, "z": 0},
                     "faces": {
                         "up": {"uv": [8, 8],"texture": c_up},
                         "down": {"uv": [8, 8],"texture": c_up},
@@ -195,14 +167,25 @@ export default class style {
                         "west": {"uv": [8, 8],"texture": c_up}
                     }
                 }
-            )
+            ])
         } else {
-            // боковой/потолочный
             parts.push(...[
+                {
+                    "size": {"x": 14, "y": 8, "z": 2},
+                    "translate": {"x": 0, "y": -2, "z": 0},
+                    "faces": {
+                        "up": {"uv": [8, 8],"texture": c_up},
+                        "down": {"uv": [8, 8],"texture": c_up},
+                        "north": {"uv": [8, 8],"texture": c_up},
+                        "south": {"uv": [8, 8],"texture": c_up},
+                        "east": {"uv": [8, 8],"texture": c_up},
+                        "west": {"uv": [8, 8],"texture": c_up}
+                    }
+                },
                 {
                     "rot": [0, Math.PI / 4, 0],
                     "size": {"x": 0, "y": 4, "z": 3},
-                    "translate": {"x": 3, "y": 5, "z": -3},
+                    "translate": {"x": -3, "y": 5, "z": -3},
                     "faces": {
                         "east": {"uv": [4.5, 5],"texture": c_chain},
                         "west": {"uv": [4.5, 5],"texture": c_chain}
@@ -211,7 +194,7 @@ export default class style {
                 {
                     "rot": [0, -Math.PI / 4, 0],
                     "size": {"x": 0, "y": 2, "z": 3},
-                    "translate": {"x": -3, "y": 3, "z": -3},
+                    "translate": {"x": -3, "y": 3, "z": 3},
                     "faces": {
                         "east": {"uv": [4.5, 10],"texture": c_chain},
                         "west": {"uv": [4.5, 10],"texture": c_chain}
@@ -220,7 +203,7 @@ export default class style {
                 {
                     "rot": [0, Math.PI / 4, 0],
                     "size": {"x": 0, "y": 4, "z": 3},
-                    "translate": {"x": -3, "y": 5, "z": 3},
+                    "translate": {"x": 3, "y": 5, "z": 3},
                     "faces": {
                         "east": {"uv": [4.5, 5],"texture": c_chain},
                         "west": {"uv": [4.5, 5],"texture": c_chain}
@@ -229,7 +212,7 @@ export default class style {
                 {
                     "rot": [0, -Math.PI / 4, 0],
                     "size": {"x": 0, "y": 2, "z": 3},
-                    "translate": {"x": 3, "y": 3, "z": 3},
+                    "translate": {"x": 3, "y": 3, "z": -3},
                     "faces": {
                         "east": {"uv": [4.5, 10],"texture": c_chain},
                         "west": {"uv": [4.5, 10],"texture": c_chain}
@@ -239,7 +222,7 @@ export default class style {
             if (rotate.y == 0) {
                 parts.push(
                     {
-                        "size": {"x": 4, "y": 2, "z": 16},
+                        "size": {"x": 16, "y": 2, "z": 4},
                         "translate": {"x": 0, "y": 7, "z": 0},
                         "faces": {
                             "up": {"uv": [8, 8],"texture": c_up},
@@ -256,7 +239,7 @@ export default class style {
                     {
                         "rot": [0, -Math.PI / 4, 0],
                         "size": {"x": 0, "y": 2, "z": 3},
-                        "translate": {"x": -3, "y": 7, "z": -3},
+                        "translate": {"x": -3, "y": 7, "z": 3},
                         "faces": {
                             "east": {"uv": [4.5, 1],"texture": c_chain},
                             "west": {"uv": [4.5, 1],"texture": c_chain}
@@ -265,7 +248,7 @@ export default class style {
                     {
                         "rot": [0, -Math.PI / 4, 0],
                         "size": {"x": 0, "y": 2, "z": 3},
-                        "translate": {"x": 3, "y": 7, "z": 3},
+                        "translate": {"x": 3, "y": 7, "z": -3},
                         "faces": {
                             "east": {"uv": [4.5, 1],"texture": c_chain},
                             "west": {"uv": [4.5, 1],"texture": c_chain}
@@ -273,76 +256,9 @@ export default class style {
                     }
                 ])
             }
-        }*/
-
-
-/*
-
-        parts.push(...[
-            {
-                "size": {"x": 4, "y": 2, "z": 16},
-                "translate": {"x": 0, "y": 7, "z": 0},
-                "faces": {
-                    "up": {"uv": [8, 8],"texture": c_up},
-                    "down": {"uv": [8, 8],"texture": c_up},
-                    "north": {"uv": [8, 8],"texture": c_up},
-                    "south": {"uv": [8, 8],"texture": c_up},
-                    "east": {"uv": [8, 8],"texture": c_up},
-                    "west": {"uv": [8, 8],"texture": c_up}
-                }
-            },
-            {
-                "rot": [0, Math.PI / 4, 0],
-                "size": {"x": 0, "y": 4, "z": 3},
-                "translate": {"x": 3, "y": 5, "z": -3},
-                "faces": {
-                    "east": {"uv": [4.5, 5],"texture": c_chain},
-                    "west": {"uv": [4.5, 5],"texture": c_chain}
-                }
-            },
-            {
-                "rot": [0, -Math.PI / 4, 0],
-                "size": {"x": 0, "y": 2, "z": 3},
-                "translate": {"x": -3, "y": 3, "z": -3},
-                "faces": {
-                    "east": {"uv": [4.5, 10],"texture": c_chain},
-                    "west": {"uv": [4.5, 10],"texture": c_chain}
-                }
-            },
-            {
-                "rot": [0, Math.PI / 4, 0],
-                "size": {"x": 0, "y": 4, "z": 3},
-                "translate": {"x": -3, "y": 5, "z": 3},
-                "faces": {
-                    "east": {"uv": [4.5, 5],"texture": c_chain},
-                    "west": {"uv": [4.5, 5],"texture": c_chain}
-                }
-            },
-            {
-                "rot": [0, -Math.PI / 4, 0],
-                "size": {"x": 0, "y": 2, "z": 3},
-                "translate": {"x": 3, "y": 3, "z": 3},
-                "faces": {
-                    "east": {"uv": [4.5, 10],"texture": c_chain},
-                    "west": {"uv": [4.5, 10],"texture": c_chain}
-                }
-            },
-            {
-                "size": {"x": 2, "y": 10, "z": 14},
-                "translate": {"x": 0, "y": -3, "z": 0},
-                "faces": {
-                    "up": {"uv": [8, 8],"texture": c_up},
-                    "down": {"uv": [8, 8],"texture": c_up},
-                    "north": {"uv": [8, 8],"texture": c_up},
-                    "south": {"uv": [8, 8],"texture": c_up},
-                    "east": {"uv": [8, 8],"texture": c_up},
-                    "west": {"uv": [8, 8],"texture": c_up}
-                }
-            }
-        ])
-*/
+        }
         matrix = mat4.create()
-        // висит на верху, значит разворачиваем к лицу
+        // разворачиваем к лицу
         if (rotate.y == 0) {
             const angle = rotate.z == 1 ? rotate.x + 1 : rotate.x + 2
             matrix = fromMat3(new Float32Array(16), CubeSym.matrices[angle % 4])
@@ -360,84 +276,13 @@ export default class style {
             })
         }
 
-        /*
-        // Texture
-        const c = bm.calcMaterialTexture(block.material, DIRECTION.UP);
-
-        const draw_bottom = block.rotate.y != 0;
-
-        const aabb = style.makeAABBSign(block, x, y, z)
-
-        if(draw_bottom) {
-            matrix = mat4.create();
-            mat4.rotateY(matrix, matrix, ((block.rotate.x - 2) / 4) * (2 * Math.PI))
-        } else {
-            matrix = CubeSym.matrices[CubeSym.dirAdd(Math.floor(block.rotate.x), CubeSym.ROT_Y2)]
-        }
-
-        // Center
-        let aabb_down;
-        if(draw_bottom) {
-            aabb_down = new AABB();
-            aabb_down.set(
-                x + .5 - CENTER_WIDTH/2,
-                y,
-                z + .5 - CENTER_WIDTH/2,
-                x + .5 + CENTER_WIDTH/2,
-                y + BOTTOM_HEIGHT,
-                z + .5 + CENTER_WIDTH/2,
-            );
-        }
-
-        // Push vertices
-        pushAABB(
-            vertices,
-            aabb,
-            pivot,
-            matrix,
-            {
-                up:     new AABBSideParams(c, 0, 0, null, null, true), // flag: 0, anim: 1 implicit
-                down:   new AABBSideParams(c, 0, 0, null, null, true),
-                south:  new AABBSideParams(c, 0, 0, null, null, true),
-                north:  new AABBSideParams(c, 0, 0, null, null, true),
-                west:   new AABBSideParams(c, 0, 0, null, null, true),
-                east:   new AABBSideParams(c, 0, 0, null, null, true),
-            },
-            new Vector(x, y, z)
-        );
-
-        if(draw_bottom) {
-            // Push vertices down
-            const c_down = bm.calcMaterialTexture(block.material, DIRECTION.DOWN);
-            pushAABB(
-                vertices,
-                aabb_down,
-                pivot,
-                matrix,
-                {
-                    up:     new AABBSideParams(c_down, 0, 1, null, null, true), // flag: 0, anim: 1 implicit
-                    down:   new AABBSideParams(c_down, 0, 1, null, null, true),
-                    south:  new AABBSideParams(c_down, 0, 1, null, null, true),
-                    north:  new AABBSideParams(c_down, 0, 1, null, null, true),
-                    west:   new AABBSideParams(c_down, 0, 1, null, null, true),
-                    east:   new AABBSideParams(c_down, 0, 1, null, null, true),
-                },
-                new Vector(x, y, z)
-            );
-        }
-
-        const text_block = style.makeTextBlock(block, aabb, pivot, matrix, x, y, z)
-        if(text_block) {
-            return [text_block]
-        }*/
-
         const aabb = style.makeAABBSign(block, x, y, z)
         const text_block = style.makeTextBlock(block, aabb, pivot, matrix, x, y, z)
         if(text_block) {
             return [text_block]
         }
 
-        return null;
+        return null
 
     }
 
@@ -455,12 +300,14 @@ export default class style {
             z + .5 + CONNECT_Z / 2,
         )
 
-        if(tblock.rotate.y == 0) {
+        if (tblock.rotate.y == 0) {
             if (tblock.rotate.z == -1) {
                 aabb.translate(0, -(.2 + aabb.height) / 2, .5 - aabb.depth / 2)
             } else {
                 aabb.translate(0, -.45, 0)
             }
+        } else if (tblock.rotate.y == -1) {
+            aabb.translate(0, -.45, 0)
         }
 
         return aabb
