39d2ff9258feecdd5e33ec6a9d1f5c65237c440f
kr0n4ik <kr0n4ik@hotmail.com>
2023-05-25 22:12:46 +0300

    add only ground

--- node_server/ticker/random/tina.ts
+++ node_server/ticker/random/tina.ts
@@ -3,6 +3,7 @@ import { Helpers, Vector } from "@client/helpers.js";
 import { BLOCK_ACTION } from "@client/server_client.js";
 import { TBlock } from "@client/typed_blocks3.js";
 import type { WorldAction } from "@client/world_action.js";
+import type { ServerChunk } from "server_chunk";
 import type { ServerWorld } from "server_world.js";
 
 const _rnd_pos = new Vector(0, 0, 0);
@@ -10,10 +11,10 @@ const tmpTBlockRnd = new TBlock()
 const _chunk_addr = new Vector()
 const SWAMP_ID = 6
 
-// tickerRandomGrassBlock
+// tickerRandomTinaBlock
 export default function randomTicker(world: ServerWorld, actions: WorldAction, world_light: int, tblock: TBlock): void {
 
-    _rnd_pos.copyFrom(tblock.posworld).addScalarSelf(Helpers.getRandomInt(-1, 1), Helpers.getRandomInt(-2, 2), Helpers.getRandomInt(-1, 1) )
+    _rnd_pos.copyFrom(tblock.posworld).addScalarSelf(Helpers.getRandomInt(-1, 1), 0, Helpers.getRandomInt(-1, 1) )
     const grid = world.chunkManager.grid
     grid.getChunkAddr(tblock.posworld.x, tblock.posworld.y, tblock.posworld.z, _chunk_addr)
     const chunk = world.chunkManager.getChunk(_chunk_addr)
@@ -32,9 +33,18 @@ export default function randomTicker(world: ServerWorld, actions: WorldAction, w
         if (above?.id == 0 && above.fluid == 0) {
             const under = world.getBlock(_rnd_pos.offset(0, -1, 0), tmpTBlockRnd)
             if (under?.id == 0 && (under.fluid & FLUID_TYPE_MASK) === FLUID_WATER_ID) {
-                actions.addBlocks([
-                    {pos: _rnd_pos.clone(), item: {id: tblock.id}, action_id: BLOCK_ACTION.REPLACE}
-                ])
+                // находим глубину
+                const pos = _rnd_pos.clone()
+                for (let i = 0; i < 3; i++) {
+                    pos.y--
+                    const block = world.getBlock(pos, tmpTBlockRnd)
+                    if (block.id != 0 ) {
+                        actions.addBlocks([
+                            {pos: _rnd_pos.clone(), item: {id: tblock.id}, action_id: BLOCK_ACTION.REPLACE}
+                        ])
+                        return
+                    }
+                }
             }
         }
     }
