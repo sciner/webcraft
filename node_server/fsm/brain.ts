98706fbe402bd0d48922d0bcbcb7dff307432dec
ExcuseMe <ExcuseMe@users.noreply.github.com>
2023-05-28 20:14:42 +0300

    fix

--- node_server/fsm/brain.ts
+++ node_server/fsm/brain.ts
@@ -336,7 +336,7 @@ export class FSMBrain {
         if (this.target || !this.targets || this.targets.length == 0 || this.distance_view < 1) {
             return
         }
-        // это медленная фекция, но ее можно выполнять раз в несколько секунд
+        // это медленная функция, но ее можно выполнять раз в несколько секунд
         if ((this.world.ticks_stat.number + this.mob.id) % 30 !== 0) {
             return
         }
