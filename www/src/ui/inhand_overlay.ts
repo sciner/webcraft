0c9e757864218564613d0c8baa27de1eda642370
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2023-07-13 16:46:53 +0300

    Fix first-person item rotation

--- www/src/ui/inhand_overlay.ts
+++ www/src/ui/inhand_overlay.ts
@@ -385,23 +385,23 @@ export class InHandOverlay {
             if(bb_display && bbmodel_hand) {
                 base.position[0] -= 1.75 // право/лево
                 base.position[1] += 0.85 // вниз/вверх
-                base.position[2] += 1.0  // на себя/от себя
+                base.position[2] += 0.5  // на себя/от себя
                 if(block.diagonal) {
                     base.position[1] += .25
                 }
                 // rotation
-                base.rotation[1] += 180 // поворот
+                //base.rotation[1] += 180 // поворот
                 // 1. position (1 = 1/16)
                 if(bbmodel_hand.translation) {
                     base.position[0] += bbmodel_hand.translation[0] / 16
                     base.position[1] += bbmodel_hand.translation[1] / 16
-                    base.position[2] += bbmodel_hand.translation[2] / 16
+                    base.position[2] -= bbmodel_hand.translation[2] / 16
                 }
                 // 2. pivot
                 // 3. rotation (в градусах -180...180)
                 if(bbmodel_hand.rotation) {
-                    base.rotation[0] += bbmodel_hand.rotation[0]
-                    base.rotation[1] += bbmodel_hand.rotation[1]
+                    base.rotation[0] -= bbmodel_hand.rotation[0]
+                    base.rotation[1] -= bbmodel_hand.rotation[1]
                     base.rotation[2] += bbmodel_hand.rotation[2]
                 }
                 // 4. scale
@@ -411,10 +411,6 @@ export class InHandOverlay {
                 quat.fromEuler(q, base.rotation[0], base.rotation[1], base.rotation[2], 'xyz')
                 mat4.fromRotationTranslationScaleOrigin(m, q, base.position, base.scale, base.pivot)
                 mat4.multiply(modelMatrix, modelMatrix, m)
-                mat4.rotateY(modelMatrix, modelMatrix, Math.PI)
-
-                // swapMatrixYZ(modelMatrix)
-                invertMatrixZ(modelMatrix)
 
                 return
             } else {
